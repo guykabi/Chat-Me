677e47d262c80829b7841f86161ef8a71b8e52c5
unknown <guyka00@gmail.com>
2023-02-10 16:51:28 +0200

    background-messages listener + removing specific user listener

--- Socket/index.js
+++ Socket/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express() 
 const cors = require('cors')
 const http = require('http')
-const {getUser,addUsers,removeUser} = require('./utils/utils')
+const {addUsers,removeUser} = require('./utils/utils')
 const {Server} = require('socket.io')
 
 
@@ -19,57 +19,47 @@ const io =new Server(server,{
 
 
 io.on('connection', socket=>{
+
     console.log(`User connected: ${socket.id}`)
     
     //On user connect - add to the connected users
     socket.on("addUser",(userId)=>{
-        let users =  addUsers(userId,socket.id)
+        let users = addUsers(userId,socket.id)
         io.emit("getUsers",users)
     })  
 
-    socket.on('sendMessage',(message,room)=>{
-        
-        if(!room){ 
-              //To specific user
-              const user = getUser(recieverId)
-              socket.to(user?.socketId).emit('recieve-message',message)
-              console.log(`Message is: ${message.text}`);
-        }
-        else{     
-              //To chat group   
-              io.in(room).emit('recieve-message',message)
+    socket.on('sendMessage',(message,room,reciever)=>{
+              io.in(room).emit('recieve-message',{message,reciever})
+
+              //To inform the user about another in coming message
+              io.local.emit('background-message',message)
+
               console.log(`Private room message ${message.text}`);
-        } 
     })  
 
     socket.on('typing',(recieverId,userTyping,room)=>{
-        if(!room){ 
-            //To specific user
-            const user = getUser(recieverId)
-            socket.to(user?.socketId).emit('user_typing',recieverId)
-            console.log(`Message is: typing...`)
-      }
-      else{     
-            //To chat group   
+
             io.in(room).emit('user_typing',
             {message:`${userTyping} is typing...`,
-            reciever:recieverId})
+            reciever:recieverId,room})
+
             console.log(`Private room message:${userTyping} typing...`);
-      } 
+    
     })
 
     socket.on('join_room',room =>{
         socket.join(room)
+
         console.log(`User ${socket.id} joined to room ${room}`);
     })
 
     socket.on('disconnect',()=>{
         let users = removeUser(socket.id)
         io.emit("getUsers",users)
+        
         console.log(`User disconnect:${socket.id}`);
     })
 })   
-
 
 
   server.listen(3001,()=>{
