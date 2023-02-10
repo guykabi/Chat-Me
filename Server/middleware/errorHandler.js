de8bf2a6a75c1b1c1d0d028983d32bc52fcb581c
unknown <guyka00@gmail.com>
2023-02-10 16:42:54 +0200

    defining invalid refresh Token as 401 error

--- Server/middleware/errorHandler.js
+++ Server/middleware/errorHandler.js
@@ -1,7 +1,15 @@
 const errorHnadler = (err,req,resp,next)=>{
     const status = err.status || 500
     const message = err.message || 'Something went wrong'
-    return resp.status(status).json({
+    
+    if(message === 'Failed to authenticate refresh token'){
+        return resp.status(401).json({
+            success:false,
+            status:401,
+            message
+        })
+    }
+        return resp.status(status).json({
         success:false,
         status,
         message
