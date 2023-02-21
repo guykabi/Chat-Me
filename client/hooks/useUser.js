247ec2c0b44a74d21256f698ed07583354b27bcb
unknown <guyka00@gmail.com>
2023-02-21 23:42:43 +0200

    hook file for react-query reuse functions

---
+++ client/hooks/useUser.js
@@ -0,0 +1,14 @@
+import { useQuery,useMutation } from "react-query"
+import { getUserDetails } from "../utils/apiUtils"
+
+export const useGetUser = (userId,enabled=true,onSuccess,onError)=>{
+  return useQuery('user',()=>getUserDetails(userId),
+  { 
+    onSuccess,
+    onError,
+    enabled,
+    staleTime:2000,
+    refetchOnMount:false
+  })
+}  
+
