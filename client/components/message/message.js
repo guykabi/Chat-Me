a8beeff948b64e3e118956f66b499d4310c8c3a1
unknown <guyka00@gmail.com>
2023-02-21 23:36:09 +0200

    seen & unseen message logic

--- client/components/message/message.js
+++ client/components/message/message.js
@@ -1,16 +1,40 @@
-import React,{memo} from 'react'
+import React,{memo, useEffect} from 'react'
 import styles from './message.module.css'
 import { getTime } from '../../utils/utils'
+import { useMutation } from 'react-query'
+import { seenMessage,seenOnChatMessage } from '../../utils/apiUtils'
 
 const Message = ({message,own}) => {
   
+  const {mutate:switchToSeen} = useMutation(seenMessage)
+  const {mutate:switchOnChatSeen} = useMutation(seenOnChatMessage)
+
+  useEffect(()=>{
+    
+    //Only when chat is loaded and there are unseen messages
+    if(message._id){
+        if(!own && !message.seen){
+             switchToSeen(message._id)
+          }
+       } 
+    //Only when the reciever is on the chat
+    else{
+        if(!own && !message.seen){  
+           switchOnChatSeen(message.conversationId)
+         }
+       }
+  },[])
+
   return (
     <>
     <div className={styles.mainMessageDiv}>
       <div className={styles.contentWrapper}>
          <div className={own?styles.ownMessage:styles.otherMessage}>
                <div>{message.text}</div>
-              <span>{message.createdAt?getTime(message.createdAt):message.time}</span>
+               <span>{message.createdAt?
+               getTime(message.createdAt):
+               message.time}
+               </span>
          </div>  
       </div>
     </div>
