e0fc047d6020ef92376e54a8516e76dd2e1a982e
unknown <guyka00@gmail.com>
2023-02-25 17:15:28 +0200

    fixing destructuring of request object - cookie

--- Server/middleware/auth.js
+++ Server/middleware/auth.js
@@ -2,20 +2,23 @@ const {verify} = require('jsonwebtoken')
 const {generateTokens} = require('../Utils/utils')
 
 const Auth = async (req,resp,next) =>{
-    
-    const {accessToken,refreshToken} = req.cookies['token']
+  
+    const accessToken = req.cookies['token']?.accessToken
+    const refreshToken = req.cookies['token']?.refreshToken
     
     if (!accessToken || !refreshToken) {
          return next(new Error('No Token Provided'))
      } 
      
-      verify(accessToken, process.env.ACCESS_SECRET_TOKEN,async (err, data)  => {
+      verify(accessToken, process.env.ACCESS_SECRET_TOKEN,async (err)  => {
       if(err) {
-        
-            verify(refreshToken, process.env.REFRESH_TOKEN,async (err, data)  => {
+       
+            verify(refreshToken, process.env.REFRESH_TOKEN,async (err)  => {
             if(err) {
+              
                return next(new Error('Failed to authenticate refresh token'))
              }
+             
                const {accessToken,refreshToken} = generateTokens()
                resp.cookie('token',{accessToken,refreshToken},{
                maxAge:process.env.COOKIE_EXPIRE_IN , httpOnly: true
