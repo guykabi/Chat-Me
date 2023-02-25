54cefeadd12d7276e5c86b8e880c1eac2c1c064b
unknown <guyka00@gmail.com>
2023-02-25 17:56:02 +0200

    connected friend listener - logic

--- client/components/onlineList/online.js
+++ client/components/onlineList/online.js
@@ -3,22 +3,66 @@ import styles from './onlineList.module.css'
 import Online from '../online/online'
 import { chatContext } from '../../context/chatContext'
 
-const OnlineList = ({socket}) => {  
+
+const OnlineList = () => {  
+
   const [onlineUsers,setOnlineUsers]=useState()
-  const {currentUser} = useContext(chatContext)
+  const {currentUser,Socket} = useContext(chatContext)
+
 
  useEffect(()=>{
-   socket?.on('getUsers',(users)=>{
-     let filterUsers = users.filter(user=>user.userId !== currentUser._id)
-     setOnlineUsers(filterUsers)
-   })
- },[socket])
+  
+    Socket.removeAllListeners('getUsers')
+
+    const handleGetUsersEvent = (users) =>{
+      
+      let allOnLineUsers = users.filter(user=>user.userId !== currentUser._id)
+      .map(u=>u.userId)
+ 
+      let onlineFriends = currentUser.friends.
+      filter(f=> allOnLineUsers.includes(f._id))
+ 
+      setOnlineUsers(onlineFriends)
+   }
+
+   Socket.on('getUsers',handleGetUsersEvent)
+   
+   
+   //When a new friend added - add him (if he is connect) to the online list!
+   const handleNotifyEvent = ({users,message}) =>{
+  
+    if(message !== 'The Friend approval has been done')return
+     
+    let allOnLinUsers = users?.map(u=>u._id)
+    let onlineFriends =  currentUser.friends
+    ?.filter(f=> allOnLinUsers.includes(f._id))
+    
+    setOnlineUsers(onlineFriends)
+   } 
+
+   Socket.on('incoming-notification',handleNotifyEvent)
+   return () => Socket.off('incoming-notification',handleNotifyEvent)
+  
+ },[Socket])
+
+
+ useEffect(()=>{
+  //When there is a change on the user's friends
+  if(!onlineUsers)return 
+     
+     let allOnLinUsers = onlineUsers?.map(u=>u._id)
+     let onlineFriends =  currentUser.friends
+     ?.filter(f=> allOnLinUsers.includes(f._id))
+     
+     setOnlineUsers(onlineFriends)
+
+ },[currentUser.friends])
 
 
   return (
     <div className={styles.onlineListWrapper}>
-      {onlineUsers?.map((user,index)=>(
-              <Online key={index} user={user}/>
+      {onlineUsers?.map((user)=>(
+              <Online key={user._id} user={user}/>
       ))}
         
     </div>
