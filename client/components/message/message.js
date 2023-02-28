c939c31eb18f1355be5edd432f4d009cd4e44520
unknown <guyka00@gmail.com>
2023-02-28 06:43:59 +0200

    click outside method + operation pop window

--- client/components/message/message.js
+++ client/components/message/message.js
@@ -1,43 +1,56 @@
-import React,{memo, useEffect} from 'react'
+import React,{memo, useEffect,useRef,useState} from 'react'
 import styles from './message.module.css'
 import { getTime } from '../../utils/utils'
 import { useMutation } from 'react-query'
-import { seenMessage,seenOnChatMessage } from '../../utils/apiUtils'
+import { seenMessage} from '../../utils/apiUtils'
+import MessageOperations from './messageOperation/messageOperations'
 
 const Message = ({message,own}) => {
-  
+
+  const [openMessageMenu,setOpenMessageMenu]=useState(false)
+  const menuRef = useRef(null)
   const {mutate:switchToSeen} = useMutation(seenMessage)
-  const {mutate:switchOnChatSeen} = useMutation(seenOnChatMessage)
 
   useEffect(()=>{
-    
-    //Only when chat is loaded and there are unseen messages
-    if(message._id){
-        if(!own && !message.seen){
-             switchToSeen(message._id)
-          }
-       } 
-    //Only when the reciever is on the chat
-    else{
-        if(!own && !message.seen){  
-           switchOnChatSeen(message.conversationId)
-         }
-       }
-  },[])
+        //Checks if message is unseen
+        if(own && message.seen)return
+          switchToSeen(message._id)  
+  },[])  
+
+  
+  const handleMessageOperation = () =>{
+      if(!own)return 
+      setOpenMessageMenu(!openMessageMenu)
+  }  
+
+  const closeOpenMenus = (e)=>{
+    if(menuRef.current && openMessageMenu && !menuRef.current.contains(e.target)){
+      setOpenMessageMenu(false)
+    }
+}
+
+document.addEventListener('mousedown',closeOpenMenus)
+
 
   return (
     <>
-    <div className={styles.mainMessageDiv}>
-      <div className={styles.contentWrapper}>
+   
+    <div className={styles.mainMessageDiv} ref={own?menuRef:null}>
+      <div 
+      className={styles.contentWrapper}
+      onClick={handleMessageOperation}>
          <div className={own?styles.ownMessage:styles.otherMessage}>
-               <div>{message.text}</div>
-               <span>{message.createdAt?
-               getTime(message.createdAt):
-               message.time}
-               </span>
+                <div dir='auto' className={styles.messageTextDiv}>
+                {message.text}
+                </div>
+                <span>
+                {getTime(message.createdAt)}
+                </span>
          </div>  
+         {openMessageMenu&&<MessageOperations/>}
       </div>
     </div>
+
     </>
   )
 }
