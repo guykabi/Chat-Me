7287ed554917fba628ae47f9f7788c8f2b9661b1
unknown <guyka00@gmail.com>
2023-02-28 06:10:59 +0200

    abbreviate for excluded populated fields

--- Server/Utils/utils.js
+++ Server/Utils/utils.js
@@ -1,12 +1,8 @@
 const {compare} = require('bcryptjs')
 const {sign} = require('jsonwebtoken')
+const {User} =require('../models/messagesModel')
+const excludeFields = '-password -friends -friendsWaitingList -notifications -__v'
 
-
-const excludePassword = (obj)=>{
-   let newObj = {...obj._doc}
-   delete newObj['password']
-   return newObj
-}  
 
 
 const generateInitialToken = async (data,password)=>{
@@ -46,6 +42,30 @@ const generateTokens = ()=>{
       {expiresIn:process.env.REFRESH_TOKEN_EXPIRE}
       )  
    return {accessToken,refreshToken}
+} 
+
+
+const approveFriend =async (id,friendId,next) =>{
+  
+      try{   
+            //Delete the pending friendship request + notification
+            let user = await User.findOneAndUpdate(                //Identifies notification via frindId              
+            { _id: id }, { $pull: { friendsWaitingList: friendId , notifications:{sender:friendId} }},{new: true})
+            .select('-password')
+            .populate({path:'friends',select:excludeFields})
+            .populate({path:'friendsWaitingList',select:excludeFields})
+
+         
+            //Adding the user to the user's array of friends!
+            await User.findOneAndUpdate(
+            { _id: friendId }, { $push: { friends: id } }) 
+  
+            let result = {message:'The Friend approval has been done',user}
+            return result
+      
+      }catch(err){
+          return next(err)
+      }
 }
 
-module.exports = {excludePassword, generateInitialToken,generateTokens}
+module.exports = {generateInitialToken,generateTokens,approveFriend}
