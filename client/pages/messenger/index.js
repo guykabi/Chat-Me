5ad07e8830f01069a6378ab3757fbc8b16a65146
unknown <guyka00@gmail.com>
2023-03-02 12:14:15 +0200

    implementing useGetUser hook+removing token extraction

--- client/pages/messenger/index.js
+++ client/pages/messenger/index.js
@@ -1,74 +1,66 @@
-import React,{ useEffect, useState,useContext,useRef} from 'react'
+import React,{ useEffect,useContext} from 'react'
 import {chatContext} from '../../context/chatContext'
 import styles from './messenger.module.css'
-import {io} from 'socket.io-client'
 import Chat from '../../components/chat/chat'
 import Conversations from '../../components/conversations/conversations'
 import Navbar from '../../components/navbar/navbar'
 import OnlineList from '../../components/onlineList/online'
-import { getConversations} from '../../utils/apiUtils'
-import { useQuery } from 'react-query'
-import { exctractCredentials, loginRedirectOnError,needToReSign } from '../../utils/utils'
+import { exctractCredentials, onError } from '../../utils/utils'
+import { useGetUser } from '../../hooks/useUser'
 
 
-const Messenger = ({hasError,user,tokens}) => {
-    const [socket, setSocket] = useState(null);
-    const {currentUser,currentChat,dispatch} = useContext(chatContext) 
+const Messenger = ({hasError,user}) => {
     
-    const {data:conversations,error} = useQuery('conversations',
-          ()=>getConversations(user._id,tokens),{refetchInterval:30000}) 
-
+    const {currentUser,currentChat,Socket,dispatch} = useContext(chatContext)
+    const {data,error} = useGetUser(user._id) 
+  
     useEffect(()=>{
-      if(!currentUser){ 
-        dispatch({ type:"CURRENT_USER", payload:user })
-      } 
-    },[])
-     
-    
-    
-    useEffect(() => {
-      const newSocket = io('http://localhost:3001');
-      setSocket(newSocket);  
-      newSocket.emit('addUser',user?._id)
-        return () => newSocket.close();
-    }, [setSocket]); 
+      
+      if(!currentUser && data){ 
+        dispatch({ type:"CURRENT_USER", payload:data })
+       } 
 
+      if(!currentUser) return
+         Socket?.emit('addUser',user._id)
 
-  //When no token provided
-  if(hasError){
-   return loginRedirectOnError()
+    },[data,currentUser])
+      
+   
+  if(hasError || error){
+    if(error)return onError('Connection problem...')
+    
+    //When no token provided 
+    return onError()
   } 
 
-  if(error){
-    if(error?.response?.data?.message === 'Failed to authenticate refresh token'){
-          return needToReSign(user.name)
-    }
-    return loginRedirectOnError('Connection error...')
-  }
       
   return (
-    <div className={styles.messangerWrapper}>
+    <>
+    {currentUser?<div className={styles.messangerWrapper}>
       <Navbar/>
-      {currentUser?<div className={styles.innerWrapper}>
+      <div className={styles.innerWrapper}>
 
-          {conversations&&<div className={styles.conversationsWrapper}>
-            <span className='threeDots'></span>
+          <div className={styles.conversationsWrapper}>
+             <span className='threeDots'></span>
              <h2>Conversations</h2><br/> 
-             <Conversations conversations={conversations}/>
-          </div>}
+             <Conversations/>
+          </div>
 
           <div className={styles.chatWrapper}>
-             {currentChat?<Chat socket={socket}/>:
-             <div className={styles.noChatDiv}>Open chat to start talk...</div>}
+             {currentChat?<Chat/>:
+             <div className={styles.noChatDiv}>
+             Open chat to start talk...
+             </div>}
           </div>
 
           <div className={styles.onlineWrapper}>
              <h2>Online</h2><br/>
-             <OnlineList socket={socket}/>
+             <OnlineList/>
           </div>
 
-       </div>:<div className='center'><h2>Loading...</h2></div>}
-    </div>
+       </div>
+    </div>:<div className='center'><h2>Loading...</h2></div>}
+   </>
   )
 } 
 
@@ -77,17 +69,10 @@ export async function getServerSideProps({req}) {
   if(!req.headers.cookie){
     return{props:{hasError:true}}
   }
-    const {user,tokensObj} = exctractCredentials(req)
-    /*let conversations;
-
-    try{
-       conversations = await getConversations(user._id,tokensObj)
-     }catch(err){
-       return {props:{hasError:true}}
-    }*/
+    const user = exctractCredentials(req) 
      
     return{
-      props:{user,tokens:tokensObj}
+      props:{user}
     }
 }
 
