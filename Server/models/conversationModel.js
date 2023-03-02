431f138fa48c178b4d295c554517d79eca78a2e6
unknown <guyka00@gmail.com>
2023-03-02 11:10:57 +0200

    exporting schema for using as a ref on messages model

--- Server/models/conversationModel.js
+++ Server/models/conversationModel.js
@@ -2,13 +2,17 @@ const {Schema,model} = require('mongoose')
 
 const ConversationSchema = new Schema({
     chatName:String, //For chat group
-    manager:{type:Schema.Types.ObjectId,ref:'users'},
+    manager:{type:Schema.Types.ObjectId,ref:'users'}, //For chat group
     participants:[{type:Schema.Types.ObjectId,ref:'users'}],
-    //Media consider as message that contains image - checked in client
+    //Media consider as message that contains image
     media:[{type:Schema.Types.ObjectId,ref:'messages'}],
+    //unSeenCount:[{type:Schema.Types.ObjectId,ref:'messages'}],
     lastActive:Date
 },
     {timestamps:true}
 )
 
-module.exports = model('conversations',ConversationSchema)
+const Conversation = model('conversations',ConversationSchema)
+
+//Also for the ref(populate) of messageModel
+module.exports = {Conversation,ConversationSchema}
