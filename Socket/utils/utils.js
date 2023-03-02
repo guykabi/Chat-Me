6b1726a5b2d849dcf5181f6fb380331afd8cea36
unknown <guyka00@gmail.com>
2023-03-02 11:29:50 +0200

    getUser/s helpers functions

--- Socket/utils/utils.js
+++ Socket/utils/utils.js
@@ -3,17 +3,26 @@ let users = []
  const addUsers = (userId,socketId)=>
 {    
     if(!userId) return
+    //Checking if user is already exists/connect
     !users.some((user)=>user.userId === userId)&&
-     users.push({userId,socketId})
-     console.log(users);
-     return users
+    users.push({userId,socketId})
+    return users
 }  
 
  const removeUser = (socketId)=>{
     users = users.filter(user=>user.socketId !==socketId)
     return users
+}  
+
+const getUser = (id) =>{
+   let foundUser = users.find(u=>u.userId === id)  
+   return foundUser
 } 
+
+const getAllUsers = () =>{
+    return users
+}
 
  
 
-module.exports = {addUsers,removeUser}
+module.exports = {addUsers,removeUser,getUser,getAllUsers}
