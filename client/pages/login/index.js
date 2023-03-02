f7e073a2c8c76811e8211964ab0a9e6dc346d84a
unknown <guyka00@gmail.com>
2023-03-02 12:24:08 +0200

    handling user redirected to login page

--- client/pages/login/index.js
+++ client/pages/login/index.js
@@ -1,31 +1,48 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState, useContext } from 'react'
+import {chatContext} from '../../context/chatContext'
 import styles from './login.module.css'
 import Input from '../../components/UI/Input/Input'
 import {checkUser} from '../../utils/apiUtils'
 import { useMutation } from 'react-query'
 import { Loader } from '../../components/UI/clipLoader/clipLoader'
-import {useRouter} from 'next/router'
+import {push} from 'next/router'
 
-const Login = () => {
+
+const Login = () => { 
+
+  const {Socket,dispatch,currentUser} = useContext(chatContext)
   const [email,setEmail]=useState('')
   const [password,setPassword]=useState('')
   const [loginMessage,setLoginMessage]=useState(null)
-  const {push} =useRouter()
 
   const {mutate:authUser,isLoading,isError}=useMutation(checkUser,{
     onSuccess:(data)=>{  
+
+      //If invalid details was inserted
       if(typeof data === "string"){
+
         setLoginMessage(data)
         let timer =  setTimeout(()=>{
           setLoginMessage(null)
         },3000)
+
         return () => {clearTimeout(timer)}
       }
-      if(data.message){
+
+      if(!data.message)return
         push('/messenger')
-      }
+      
     }
-  })
+  }) 
+
+  useEffect(()=>{
+     if(!Socket)return
+      
+     Socket.disconnect()
+     dispatch({type:'SOCKET',payload:null})
+
+  },[currentUser])
+
 
   const sendCredentials =(e)=>{
     e.preventDefault()
@@ -55,7 +72,7 @@ const Login = () => {
                  require
                  onChange={(e)=>setPassword(e.target.value)}
                  /> 
-                 {isLoading?<Loader/>:null}
+                 {isLoading?<Loader size={22}/>:null}
                  {loginMessage?<span>{loginMessage}</span>:null}
                  {isError?<span>Something went wrong!</span>:null}
                  <br/><br/>
