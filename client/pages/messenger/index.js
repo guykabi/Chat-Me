65dff725a3386b0fa2b4df200d7fa45cf4c7052e
Guy Kabilio <guyka00@gmail.com>
2023-03-14 11:11:59 +0200

    adding popup menu to switch create-group section

--- client/pages/messenger/index.js
+++ client/pages/messenger/index.js
@@ -1,113 +1,126 @@
-import React,{ useEffect,useContext,useState} from 'react'
-import {chatContext} from '../../context/chatContext'
-import styles from './messenger.module.css'
-import Chat from '../../components/chat/chat'
-import Conversations from '../../components/conversations/conversations'
-import Navbar from '../../components/navbar/navbar'
-import OnlineList from '../../components/onlineList/online'
-import { exctractCredentials, onError } from '../../utils/utils'
-import { useGetUser } from '../../hooks/useUser'
-import CreateGroup from '../../components/createGroup/createGroup'
+import React, { useEffect, useContext, useState } from "react";
+import { chatContext } from "../../context/chatContext";
+import styles from "./messenger.module.css";
+import Chat from "../../components/chat/chat";
+import Conversations from "../../components/conversations/conversations";
+import Navbar from "../../components/navbar/navbar";
+import OnlineList from "../../components/onlineList/online";
+import { exctractCredentials, onError } from "../../utils/utils";
+import { useGetUser } from "../../hooks/useUser";
+import CreateGroup from "../../components/createGroup/createGroup";
 
+const Messenger = ({ hasError, user }) => {
+  const { currentUser, currentChat, Socket, dispatch } =
+    useContext(chatContext);
+  const { data, error } = useGetUser(user._id);
 
-const Messenger = ({hasError,user}) => {
-    
-    const {currentUser,currentChat,Socket,dispatch} = useContext(chatContext)
-    const {data,error} = useGetUser(user._id) 
-    
-    const [openMenu,setOpenMenu] = useState(false)
-    const [openCreateGroup,setOpenCreateGroup]=useState(false)
-    
-  
-    useEffect(()=>{
-      
-      if(!currentUser && data){ 
-        dispatch({ type:"CURRENT_USER", payload:data })
-       } 
+  const [openMenu, setOpenMenu] = useState(false);
+  const [openCreateGroup, setOpenCreateGroup] = useState(false);
 
-      if(!currentUser) return
-         Socket?.emit('addUser',user._id)
-
-    },[data,currentUser]) 
-
-    const handleOpenCreateGroup = () =>{
-      setOpenCreateGroup(true)
-      setOpenMenu(false)
-    } 
-
-    const handleCloseCreateGroup = () =>{
-       setOpenCreateGroup(false)
-       setOpenMenu(false)
+  useEffect(() => {
+    if (!currentUser && data) {
+      dispatch({ type: "CURRENT_USER", payload: data });
     }
-      
-   
-  if(hasError || error){
-    if(error)return onError('Connection problem...')
-    
-    //When no token provided 
-    return onError()
-  } 
 
-      
+    if (!currentUser) return;
+    Socket?.emit("addUser", user._id);
+  }, [data, currentUser]);
+
+  const handleOpenCreateGroup = () => {
+    setOpenCreateGroup(true);
+    setOpenMenu(false);
+  };
+
+  const handleCloseCreateGroup = () => {
+    setOpenCreateGroup(false);
+    setOpenMenu(false);
+  };
+
+  if (hasError || error) {
+    if (error) return onError("Connection problem...");
+
+    //When no token provided
+    return onError();
+  }
+
   return (
     <>
-    {currentUser?<div className={styles.messangerWrapper}>
-      <Navbar/>
-      <div className={styles.innerWrapper}>
+      {currentUser ? (
+        <section className={styles.messangerWrapper}>
+          <Navbar />
+          <div className={styles.innerWrapper}>
+            <div
+              className={
+                openCreateGroup
+                  ? styles.createGroupsWrapper
+                  : styles.conversationsWrapper
+              }
+            >
+              <span
+                className="threeDots"
+                role="button"
+                onClick={() => setOpenMenu(!openMenu)}
+              ></span>
 
-      <div className={openCreateGroup?
-           styles.createGroupsWrapper:
-           styles.conversationsWrapper}>
+              {openMenu && (
+                <div className={styles.popupMenuConversations}>
+                  <div onClick={handleOpenCreateGroup} role="button">
+                    Create group
+                  </div>
+                  <div onClick={handleCloseCreateGroup} role="button">
+                    All conversations
+                  </div>
+                </div>
+              )}
 
-         <span 
-         className='threeDots'
-         onClick={()=>setOpenMenu(!openMenu)}></span> 
+              {openCreateGroup ? (
+                <article>
+                  <CreateGroup onSwitch={handleCloseCreateGroup} />
+                </article>
+              ) : (
+                <article>
+                  <h2>Conversations</h2>
+                  <br />
+                  <Conversations />
+                </article>
+              )}
+            </div>
 
-        {openMenu&&
-        <div className={styles.popupMenuConversations}>
-           <div onClick={handleOpenCreateGroup} role='button'>Create group</div>
-           <div onClick={handleCloseCreateGroup} role='button'>All conversations</div>
-        </div>}
+            <div className={styles.chatWrapper}>
+              {currentChat ? (
+                <Chat />
+              ) : (
+                <div className={styles.noChatDiv}>
+                  Open chat to start talk...
+                </div>
+              )}
+            </div>
 
-        {openCreateGroup?
-        <article>
-             <CreateGroup onSwitch={handleCloseCreateGroup}/>
-         </article>:
-         <article>
-            <h2>Conversations</h2><br/> 
-            <Conversations/>
-         </article>}
-
-      </div>
-
-          <div className={styles.chatWrapper}>
-             {currentChat?<Chat/>:
-             <div className={styles.noChatDiv}>
-             Open chat to start talk...
-             </div>}
+            <div className={styles.onlineWrapper}>
+              <h2>Online</h2>
+              <br />
+              <OnlineList />
+            </div>
           </div>
+        </section>
+      ) : (
+        <section className="center">
+          <h2>Loading...</h2>
+        </section>
+      )}
+    </>
+  );
+};
 
-          <div className={styles.onlineWrapper}>
-             <h2>Online</h2><br/>
-             <OnlineList/>
-          </div>
-
-       </div>
-    </div>:<div className='center'><h2>Loading...</h2></div>}
-   </>
-  )
-} 
-
-export async function getServerSideProps({req}) {
-
-  if(!req.headers.cookie){
-    return{props:{hasError:true}}
+export async function getServerSideProps({ req }) {
+  if (!req.headers.cookie) {
+    return { props: { hasError: true } };
   }
-    const user = exctractCredentials(req) 
-     
-    return{
-      props:{user}
-    }
+  const user = exctractCredentials(req);
+
+  return {
+    props: { user },
+  };
 }
 
-export default Messenger
+export default Messenger;
