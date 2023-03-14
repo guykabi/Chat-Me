4a1cbca6dc11311346e86c349413b8d2a2cd7980
Guy Kabilio <guyka00@gmail.com>
2023-03-14 20:23:38 +0200

    implementing i18-next for global languages of app

--- client/next.config.js
+++ client/next.config.js
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
 
+
+
 const nextConfig = {
   reactStrictMode: true,
   i18n: {
-    locales: ['he','en'],
-    defaultLocale: 'he',
-  },
+    locales: ["he","en"],
+    defaultLocale: "he",
+}
 }
 
 module.exports = nextConfig
