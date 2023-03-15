58561e27cd5aeb55f37a613c12bb9cac31e6ac19
Guy Kabilio <guyka00@gmail.com>
2023-03-15 14:31:44 +0200

    prettier

--- client/components/messages/messages.js
+++ client/components/messages/messages.js
@@ -1,150 +1,138 @@
-import React,{useContext, useEffect,useRef,memo, useState,useMemo} from 'react'
-import { needToReSign,onError } from '../../utils/utils'
-import styles from './messages.module.css'
-import Message from '../message/message'
-import { chatContext } from '../../context/chatContext'
-import {useMutation, useQuery} from 'react-query'
-import {getMessages,deleteConversation} from '../../utils/apiUtils'
+import React, {
+  useContext,
+  useEffect,
+  useRef,
+  memo,
+  useState,
+  useMemo,
+} from "react";
+import { needToReSign, onError } from "../../utils/utils";
+import styles from "./messages.module.css";
+import Message from "../message/message";
+import { chatContext } from "../../context/chatContext";
+import { useMutation, useQuery } from "react-query";
+import { getMessages, deleteConversation } from "../../utils/apiUtils";
 
+const Messages = ({ messages }) => {
+  const { currentUser, currentChat, Socket } = useContext(chatContext);
+  const scrollRef = useRef();
+  const [allMessages, setAllMessages] = useState([]);
+  const [amountToSkip, setAmountToSkip] = useState(30);
+  const [isMoreMessages, setIsMoreMessages] = useState();
+  const [newChatToDelete, setNewChatToDelete] = useState(null);
 
-const Messages = ({messages}) => {
-  
-    const {currentUser,currentChat,Socket} = useContext(chatContext)
-    const scrollRef = useRef()
-    const [allMessages,setAllMessages]=useState([])  
-    const [amountToSkip,setAmountToSkip]=useState(30)
-    const [isMoreMessages,setIsMoreMessages]=useState()
-    const [newChatToDelete,setNewChatToDelete]=useState(null)
-     
-    const {refetch:loadMore,error} = useQuery('more-messages',()=>(
-      getMessages(currentChat?._id,amountToSkip)),
+  const { refetch: loadMore, error } = useQuery(
+    "more-messages",
+    () => getMessages(currentChat?._id, amountToSkip),
     {
-    onSuccess:(data)=>{   
-      
-      if(data.length){
-           let reverseMessages = [...data].reverse()
-           setAllMessages(prev=> [...reverseMessages, ...prev])
-        
-         if(data.length === 30){
-           setAmountToSkip(prev=>prev+=30)
-         }
+      onSuccess: (data) => {
+        if (data.length) {
+          let reverseMessages = [...data].reverse();
+          setAllMessages((prev) => [...reverseMessages, ...prev]);
 
-         if(data.length < 30){
-           setIsMoreMessages(false)
+          if (data.length === 30) {
+            setAmountToSkip((prev) => (prev += 30));
           }
-          
-       }
-    }, 
-    enabled:false
-  })    
 
+          if (data.length < 30) {
+            setIsMoreMessages(false);
+          }
+        }
+      },
+      enabled: false,
+    }
+  );
 
-  const {mutate:removeConversation} = useMutation(deleteConversation,{
-    onSuccess:(data)=>{
-      if(data !== 'Conversation deleted!')return 
+  const { mutate: removeConversation } = useMutation(deleteConversation, {
+    onSuccess: (data) => {
+      if (data !== "Conversation deleted!") return;
 
       //Emit event to trigger the conversations-component to fetch updated conversations
-      Socket.emit('new-conversation',currentUser._id)
-    }
-  })
+      Socket.emit("new-conversation", currentUser._id);
+    },
+  });
 
-   
-   useEffect(()=>{
-       
-      //Only on chat switching - or new chat creation
-      if(!messages.length)return
-      setAllMessages(messages)
-      
-      if(messages.length === 30)setIsMoreMessages(true)
-      
-      //Reset the amount when switching chats
-      if(!(amountToSkip > 30))return
-      setAmountToSkip(30)
+  useEffect(() => {
+    //Only on chat switching - or new chat creation
+    if (!messages.length) return;
+    setAllMessages(messages);
 
-   },[messages])
+    if (messages.length === 30) setIsMoreMessages(true);
 
+    //Reset the amount when switching chats
+    if (!(amountToSkip > 30)) return;
+    setAmountToSkip(30);
+  }, [messages]);
 
-   useEffect(()=>{   
-    
+  useEffect(() => {
     //Detecting if it's a new chat without messages
-    if(!messages.length && !allMessages.length){
-      
-       //Sets the new chat in advance to delete - if no message will be send
-       setNewChatToDelete(currentChat._id)
-       return
-     }
-   
-    setNewChatToDelete(null)
-    if(!isMoreMessages)return
-      scrollRef.current?.scrollIntoView({
-      behavior:'smooth',
-      block: 'start',
-      inline: 'nearest' 
-    })   
-     
-  },[allMessages])
+    if (!messages.length && !allMessages.length) {
+      //Sets the new chat in advance to delete - if no message will be send
+      setNewChatToDelete(currentChat._id);
+      return;
+    }
 
+    setNewChatToDelete(null);
+    if (!isMoreMessages) return;
+    scrollRef.current?.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+      inline: "nearest",
+    });
+  }, [allMessages]);
 
+  useEffect(() => {
+    Socket.removeAllListeners("recieve-message");
+    Socket.on("recieve-message", ({ message }) => {
+      if (message.conversation._id !== currentChat._id) return;
 
-   useEffect(()=>{
-    
-      Socket.removeAllListeners('recieve-message')
-      Socket.on('recieve-message',({message})=>{
-       
-      if(message.conversation._id !== currentChat._id) return
-      
-      //In order to avoid messages duplication - 
+      //In order to avoid messages duplication -
       //increasing the amount of documnets to skip on when new message is added
-      setAmountToSkip( prev => prev += 1)
-      setAllMessages(prev =>[...prev,message])
-      
+      setAmountToSkip((prev) => (prev += 1));
+      setAllMessages((prev) => [...prev, message]);
+
       //If at least one message was sent - there is no need to delete new chat
-      if(!newChatToDelete)return
-      setNewChatToDelete(null)
-      }) 
+      if (!newChatToDelete) return;
+      setNewChatToDelete(null);
+    });
+  }, [Socket, newChatToDelete]);
 
-   },[Socket,newChatToDelete])
-  
+  useEffect(() => {
+    //If no message was sent in the new chat - delete it
+    if (!newChatToDelete) return;
+    setNewChatToDelete(null);
+    removeConversation(newChatToDelete);
+  }, [currentChat]);
 
-    useEffect(()=>{
-         //If no message was sent in the new chat - delete it
-         if(!newChatToDelete)return  
-         setNewChatToDelete(null)
-         removeConversation(newChatToDelete)
-    },[currentChat])    
-   
-  
+  const handleMoreLoading = (e) => {
+    if (e.target.scrollTop === 0 && isMoreMessages) {
+      loadMore();
+    }
+  };
 
-    const handleMoreLoading =(e)=>{
-        if(e.target.scrollTop === 0 && isMoreMessages){
-          loadMore()
-        }
-      }  
+  if (error) {
+    if (error?.response?.status === 401) {
+      return needToReSign(currentUser.name);
+    }
+    return onError();
+  }
 
+  const memoMessages = useMemo(() => allMessages, [allMessages]);
 
-      if(error){
-        if(error?.response?.status === 401){
-          return needToReSign(currentUser.name)
-         }
-        return onError()
-      }
-
-      
-    const memoMessages = useMemo(()=>allMessages,[allMessages])
-
-
-   
   return (
     <>
-     <section className={styles.messagesDiv} onScroll={handleMoreLoading} >  
-     {memoMessages?.map((message)=>(
-        <div key={message._id} ref={scrollRef}>
-              <Message message={message}
-               own={message.sender===currentUser._id}/>
-        </div>))}
-     </section>
+      <section className={styles.messagesDiv} onScroll={handleMoreLoading}>
+        {memoMessages?.map((message) => (
+          <div key={message._id} ref={scrollRef}>
+            <Message
+              message={message}
+              own={message.sender === currentUser._id}
+            />
+          </div>
+        ))}
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default memo(Messages)
+export default memo(Messages);
