ceeb79dc3fa9fd871ed3969aa39c1d81ec3e11b4
Guy Kabilio <guyka00@gmail.com>
2023-03-19 06:46:17 +0200

    fixing popup menu relative position

--- client/components/message/messageOperation/messageOperations.js
+++ client/components/message/messageOperation/messageOperations.js
@@ -1,12 +1,20 @@
 import React,{memo} from 'react'
 import styles from './messageOperations.module.css'
+import {AiOutlineLike} from 'react-icons/ai'
+import {BsTrash3} from 'react-icons/bs'
 
 const MessageOperations = () => {
   return (
     <>
         <div className={styles.messageMenuDiv}>
-           <div>Like message</div>
-           <div>Delete message</div>
+           <div>
+            <AiOutlineLike/>
+            Like message
+            </div>
+           <div>
+           <BsTrash3/>
+           Delete message
+           </div>
         </div>
     </>
   )
