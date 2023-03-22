6cf173d812aa2a9b924b585369070cf5ad3f70cf
Guy Kabilio <guyka00@gmail.com>
2023-03-23 00:59:06 +0200

    adding functions to handle remove/adding and set as manager

--- client/components/group-person/groupPerson.js
+++ client/components/group-person/groupPerson.js
@@ -2,45 +2,90 @@ import React,{useContext} from 'react'
 import styles from './groupPerson.module.css'
 import { chatContext } from '../../context/chatContext'
 
-const GroupPerson = ({user,onPick,manager,menu,onMenu}) => {
-  
-  const {currentChat,currentUser} = useContext(chatContext)
+const GroupPerson = (props) => {
+   const {
+    user,
+    onPick,
+    onRemove,
+    onAddManager,
+    onRemoveManager,
+    manager,
+    menu,
+    onMenu} = props
+    const {currentChat,currentUser} = useContext(chatContext)
 
 
-const memberOperations = (
-    <section className={styles.memberOperationMenu}>
-      <div role='button'>Define as manager</div>
-      <div role='button'>Remove</div>
-    </section>
-  )
-
-
-const handleOpenMenu = () =>{
+const handleOpenMenu = (e) =>{
+  e.stopPropagation();
   if(menu===user._id){
     onMenu(null)
     return
   }
   onMenu(user._id)
+} 
+
+const handlePick = (e) =>{
+  e.stopPropagation();
+  onPick(user)
+} 
+
+
+const handleRemoval = (e) =>{
+  e.stopPropagation();
+  onRemove(user._id)
 }
+
+const handleAddManager = (e) =>{
+  e.stopPropagation();
+  onAddManager(user._id)
+}
+
+const handleRemoveManager = (e) =>{
+  e.stopPropagation();
+  onRemoveManager(user._id)
+}
+
+const memberOperations = (
+  <section className={styles.memberOperationMenu}>
+
+    {manager?
+    <div 
+    role='button' 
+    onClick={handleRemoveManager}
+    >Remove manager</div>:
+    <div 
+    role='button' 
+    onClick={handleAddManager}
+    >Define as manager</div>}
+
+    <div 
+    role='button' 
+    onClick={handleRemoval}
+    >Remove</div>
+
+  </section>
+)
 
   return (
     <>
-     <article className={styles.personMainDiv} onClick={()=>onPick(user)}>
+     <article className={styles.personMainDiv} 
+     onClick={handlePick}>
 
      {menu===user._id&&memberOperations}
 
       <div className={styles.userImageWrapper}>
           <img  src={user?.image?user.image:'/images/no-avatar.png'}/>
       </div>
+
       <div className={styles.userNameWrapper}>
 
         <section>
          {user?.name} 
         </section>&nbsp;
 
-        {manager&&<span> - Manager</span>}
+        {manager?<span> - Manager</span>:null}
 
-        {currentUser._id===currentChat?.manager?._id&&
+        {currentChat?.manager.some(m=>m._id === currentUser._id)&&
          currentUser._id!==user._id&&
          manager!==undefined&&
         <div 
@@ -50,6 +95,7 @@ const handleOpenMenu = () =>{
         </div>}
 
       </div>
+      
      </article>
     </>
   )
