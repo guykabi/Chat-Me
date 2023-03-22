57c6f6765cc25fa904b58922812076d93a2a0559
Guy Kabilio <guyka00@gmail.com>
2023-03-23 00:49:31 +0200

    logics for remove/add/leaving group and define manager

--- client/components/chatDetails/chatDetails.js
+++ client/components/chatDetails/chatDetails.js
@@ -6,20 +6,33 @@ import Input from "../UI/Input/Input";
 import GroupPerson from "../group-person/groupPerson";
 import Button from "../UI/Button/button";
 import { useMutation, useQuery } from "react-query";
-import { updateConversation, getAllusers } from "../../utils/apiUtils";
 import Modal from "../Modal/modal";
+import PickedUser from "../pickedUser/pickedUser";
+import { BiSend } from "react-icons/bi";
+import {
+  updateConversation,
+  getAllusers,
+  addGroupMember,
+  removeGroupMember,
+  addManager,
+  removeManager,
+} from "../../utils/apiUtils";
 
-const EditGroup = ({ onReturn }) => {
-  const { currentChat, Socket, dispatch } = useContext(chatContext);
+const ChatDetails = ({ onReturn }) => {
+  const { currentChat,currentUser, Socket, dispatch } = useContext(chatContext);
   const [showModal, setShowModal] = useState(false);
   const [allUsers, setAllUsers] = useState([]);
   const [isGroup, setIsGroup] = useState(false);
+  const [query, setQuery] = useState("");
   const [pickedUsersToAdd, setPickedUsersToAdd] = useState([]);
   const [groupChangedDetails, setGroupChangedDetails] = useState({});
   const [isMenu, setIsMenu] = useState(null);
+  const [errorText, setErrorText] = useState(null);
+  const [addMemberErrorText, setAddMemberErrorText] = useState(null);
 
   const { refetch } = useQuery("users", getAllusers, {
     onSuccess: (data) => {
+      //Filter only unmembers of the group
       let chatMembersIds = currentChat.participants.map((p) => p._id);
       let filteredData = data.filter((u) => !chatMembersIds.includes(u._id));
       setAllUsers(filteredData);
@@ -33,8 +46,72 @@ const EditGroup = ({ onReturn }) => {
       Socket.emit("new-conversation", data.conversation);
       dispatch({ type: "CURRENT_CHAT", payload: data.conversation });
     },
-    onError: (error) => {
-      console.log(error.message);
+    onError: () => {
+      setErrorText("Unabled to edit");
+      let timer = setTimeout(() => {
+        setErrorText(null);
+      }, 3000);
+      return () => clearTimeout(timer);
+    },
+  });
+
+  const { mutate: addMember } = useMutation(addGroupMember, {
+    onSuccess: ({ message, conversation }) => {
+      if (message !== "Member added") return;
+      Socket.emit("new-conversation", conversation);
+      dispatch({ type: "CURRENT_CHAT", payload: conversation });
+    },
+    onError: () => {
+      setAddMemberErrorText("Unabled to add");
+      let timer = setTimeout(() => {
+        setAddMemberErrorText(null);
+      }, 3000);
+      return () => clearTimeout(timer);
+    },
+  });
+
+  const { mutate: removeMember } = useMutation(removeGroupMember, {
+    onSuccess: ({ message, conversation }) => {
+      if (message !== "Member removed") return;
+      Socket.emit("new-conversation", conversation);
+      dispatch({ type: "CURRENT_CHAT", payload: conversation });
+    },
+    onError: () => {
+      setErrorText("Unabled to remove");
+      let timer = setTimeout(() => {
+        setErrorText(null);
+      }, 3000);
+      return () => clearTimeout(timer);
+    },
+  });
+
+  const { mutate: setManager } = useMutation(addManager, {
+    onSuccess: ({ message, conversation }) => {
+      if (message !== "Manager added") return;
+      Socket.emit("new-conversation", conversation);
+      dispatch({ type: "CURRENT_CHAT", payload: conversation });
+    },
+    onError: () => {
+      setErrorText("Unabled to set manager");
+      let timer = setTimeout(() => {
+        setErrorText(null);
+      }, 3000);
+      return () => clearTimeout(timer);
+    },
+  }); 
+
+  const { mutate: managerRemoval } = useMutation(removeManager, {
+    onSuccess: ({ message, conversation }) => {
+      if (message !== "Manager removed") return;
+      Socket.emit("new-conversation", conversation);
+      dispatch({ type: "CURRENT_CHAT", payload: conversation });
+    },
+    onError: () => {
+      setErrorText("Unabled to remove");
+      let timer = setTimeout(() => {
+        setErrorText(null);
+      }, 3000);
+      return () => clearTimeout(timer);
     },
   });
 
@@ -63,9 +140,56 @@ const EditGroup = ({ onReturn }) => {
     setPickedUsersToAdd((prev) => [...prev, e]);
   };
 
-  const removePickedFriend = (pickedUser) => {
+  const removePickedUser = (pickedUser) => {
     setPickedUsersToAdd((prev) => prev.filter((u) => u._id !== pickedUser._id));
   };
+
+  const handleModalClose = () => {
+    setShowModal(false);
+    setPickedUsersToAdd([]);
+  };
+
+  const handleMemberAdding = () => {
+    if (!pickedUsersToAdd.length) return;
+    let obj = { participants: pickedUsersToAdd.map((p) => p._id) };
+    addMember({ conId: currentChat._id, obj });
+  };
+
+  const handleMemberRemoval = (e) => {
+    let obj;
+
+    if(typeof e !== 'string' ){
+
+      //When the only manager wants to leave the group - 
+      //Sets a random member of the group as manager
+      if(currentChat.manager.length === 1 && 
+         currentChat.manager[0]._id === currentUser._id)
+         {
+          
+         let setAsManager =  currentChat.participants.find(p=>p._id !== currentUser._id)
+         let obj = { manager: setAsManager._id };
+         setManager({ conId: currentChat._id, obj });
+         }
+
+      let obj = {participants:currentUser._id}
+      removeMember({ conId: currentChat._id, obj });
+     return
+
+    }
+
+     obj = {participants: e}
+     removeMember({ conId: currentChat._id, obj });
+  }; 
+
+  const handleManagerAdding = (e) => {
+    let obj = { manager: e };
+    setManager({ conId: currentChat._id, obj });
+  };  
+
+  const handleManagerRemoval = (e) =>{
+    let obj = { manager: e };
+    managerRemoval({ conId: currentChat._id, obj });
+  }
 
   const submitChatDetailChange = (e) => {
     e.preventDefault();
@@ -79,42 +203,31 @@ const EditGroup = ({ onReturn }) => {
         <GroupPerson
           key={user._id}
           user={user}
-          onPick={() => console.log("Hey")}
+          onRemove={handleMemberRemoval}
+          onAddManager={handleManagerAdding}
+          onRemoveManager={handleManagerRemoval}
           onMenu={(e) => setIsMenu(e)}
           menu={isMenu}
-          manager={currentChat?.manager?._id === user._id}
+          manager={currentChat?.manager.some(m=>m._id === user._id)}
         />
       )),
     [isMenu]
   );
 
-  const memoAllUsers = useMemo(
+  const allUsersToPick = useMemo(
     () =>
-      allUsers?.map((user) => {
-        return (
-          <GroupPerson
-            key={user._id}
-            user={user}
-            onPick={handleUserPick}
-            menu={isMenu}
-          />
-        );
-      }),
-    [allUsers]
+      allUsers
+        ?.filter((u) =>
+          u.name.toLowerCase().includes(query.trim().toLowerCase())
+        )
+        .map((user) => (
+          <GroupPerson key={user._id} user={user} onPick={handleUserPick} />
+        )),
+    [allUsers, query, pickedUsersToAdd]
   );
 
   const pickedNewUsers = pickedUsersToAdd.map((user) => (
-    <div key={user._id} className={styles.pickedUserShow}>
-      <img src={user?.image ? user.image : "/images/no-avatar.png"} />
-      <span aria-label="user-image">{user.name}</span>
-      <span
-        className={styles.xDelete}
-        onClick={() => removePickedFriend(user)}
-        role="button"
-      >
-        x
-      </span>
-    </div>
+    <PickedUser key={user._id} user={user} onRemove={removePickedUser} />
   ));
 
   return (
@@ -153,7 +266,7 @@ const EditGroup = ({ onReturn }) => {
               <div>{currentChat?.friend?.name}</div>
             </h2>
           )}
-
+          {errorText && <p>{errorText}</p>}
           <article className={styles.chatImageWrapper}>
             {isGroup ? (
               <img
@@ -185,46 +298,59 @@ const EditGroup = ({ onReturn }) => {
       {isGroup && (
         <section className={styles.buttonsWrapper}>
           <Button
-            width="6"
-            height="15"
+            width="8"
+            height="25"
             text="Leave group"
             className="secondaryBtn"
+            onClick={handleMemberRemoval}
           />
           <Button
             width="8"
-            height="15"
-            text="Add member"
+            height="25"
+            text="Add member" 
+            //Only manager can add
+            disabled={!currentChat?.manager.some(m=>m._id === currentUser._id)}
             className="secondaryBtn"
             onClick={handleOpenModal}
-          />
-          <Button
-            width="6"
-            height="15"
-            text="Mute group"
-            className="secondaryBtn"
           />
         </section>
       )}
 
-      <Modal show={showModal} onClose={() => setShowModal(false)}>
-        <header>Choose user</header>
-
+      <Modal show={showModal} onClose={handleModalClose}>
+        <header>
+          {addMemberErrorText ? addMemberErrorText : "Choose user"}
+        </header>
         {pickedUsersToAdd.length ? (
           <section className={styles.pickedUsersWrapper}>
             {pickedNewUsers}
+            <span
+              className={styles.sendNewMembersBtn}
+              role="button"
+              aria-label="Add picked users"
+              onClick={handleMemberAdding}
+            >
+              <BiSend />
+            </span>
           </section>
         ) : null}
 
         {allUsers.length ? (
           <section className={styles.searchInputWrapper}>
-            <Input width="50" height="30" placeholder="Search user to add" />
+            <Input
+              width="50"
+              height="30"
+              placeholder="Search user to add"
+              onChange={(e) => setQuery(e.target.value)}
+            />
           </section>
         ) : null}
 
-        <section>{allUsers.length ? memoAllUsers : <h3>No Users</h3>}</section>
+        <section>
+          {allUsers.length ? allUsersToPick : <h3>No Users</h3>}
+        </section>
       </Modal>
     </main>
   );
 };
 
-export default EditGroup;
+export default ChatDetails;
