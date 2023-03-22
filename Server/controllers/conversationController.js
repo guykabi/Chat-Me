3f7071f676c330159443ffda5d8072422b44f0f1
Guy Kabilio <guyka00@gmail.com>
2023-03-23 00:28:01 +0200

    counting unseen messages per conversation logic

--- Server/controllers/conversationController.js
+++ Server/controllers/conversationController.js
@@ -1,87 +1,190 @@
-const {Conversation} = require('../models/conversationModel')
-const {Message} = require('../models/messagesModel')
-const excludeFields = '-password -friends -friendsWaitingList -notifications -__v'
+const { Conversation } = require("../models/conversationModel");
+const { Message } = require("../models/messagesModel");
+const excludeFields =
+  "-password -friends -friendsWaitingList -notifications -__v";
 
+const getAllConversations = async (req, resp, next) => {
+  const { id } = req.params;
+  try {
+    let allConversations = await Conversation.find({
+      participants: { $in: { _id: id } },
+    })
+      .sort({ lastActive: -1 })
+      .populate({ path: "participants", select: excludeFields })
+      .populate({ path: "manager", select: excludeFields });
 
-const getAllConversations = async(req,resp,next)=>{
-       
-        try{
-           let allConversations =  await Conversation
-           .find({participants:{$in:{_id:req.params.id}}})
-           .sort({lastActive:-1})
-           //Exclude password&friends
-           .populate({path:'participants',select:excludeFields})
-           .populate({path:'manager',select:excludeFields})
-          
-   
-           return resp.status(200).json(allConversations)
-           
-         }catch(err){
-            next(err)
-         }
-}
+    let all = await Promise.all(
+      allConversations.map(async (con) => {
+        let newCon = { ...con._doc };
+        let temp = await Message.count({
+          conversation: con._id,
+          "sender":{$ne: id},
+          "seen.user": { $ne: id },
+        });
+        newCon.unSeen = temp;
+        return newCon;
+      })
+    );
 
+    return resp.status(200).json(all);
+    
+  } catch (err) {
+    next(err);
+  }
+};
 
-const addNewConversation = async(req,resp,next)=>{
-   const {participants} = req.body 
-   const newConversation = new Conversation(req.body)
-   
-   try{ 
+const addNewConversation = async (req, resp, next) => {
+  const { participants } = req.body;
+  const newConversation = new Conversation(req.body);
 
-      let isAlreadyConversation;
-       
-      //Only non group chats are not allowed to duplicate
-      if(!req.body?.chatName){
-        isAlreadyConversation = await Conversation.
-        find({participants:{$all:participants}})
-      }
-      
-      if(isAlreadyConversation?.length) 
-      return resp.status(200).json('Conversation already exist')
+  try {
+    let isAlreadyConversation;
 
-       let newAddedCon =  await newConversation.save()
-
-       let conversation = await Conversation
-       .findById(newAddedCon._doc._id.toString())
-       .populate({path:'manager',select:excludeFields})
-       .populate({path:'participants',select:excludeFields})
-       
-       resp.status(200).json({message:'New conversation made',conversation})
-
-   }catch(err){
-      next(err)
-   }
-}
-
-
-//Mostly for group chats
-const updateConversation = async(req,resp,next)=>{
-   const {id} = req.params 
-   const {body} = req
-   try{
-      let updateConversation = await Conversation.
-      findByIdAndUpdate(id,body,{new:true}) 
-      .populate({path:'manager',select:excludeFields})
-      .populate({path:'participants',select:excludeFields})
-      resp.status(200).json({message:'Update',conversation:updateConversation})
-   }catch(err){
-      next(err)
-   }
-}
-
-
-const deleteConversation = async(req,resp,next) =>{
-   const {id} = req.params
-    try{
-      let isDeleted = await Conversation.findByIdAndDelete(id)
-      if(isDeleted){
-         await Message.deleteMany({conversation:id})
-      }
-       resp.status(200).json('Conversation deleted!')
-    }catch(err){
-     next(err)
+    //Only non group chats are not allowed to duplicate
+    if (!req.body?.chatName) {
+      isAlreadyConversation = await Conversation.find({
+        participants: { $all: participants },
+      });
     }
-}
 
-module.exports = {getAllConversations,addNewConversation,
-                  updateConversation,deleteConversation }
+    if (isAlreadyConversation?.length)
+      return resp.status(200).json("Conversation already exist");
+
+    let newAddedCon = await newConversation.save();
+
+    let conversation = await Conversation.findById(
+      newAddedCon._doc._id.toString()
+    )
+      .populate({ path: "manager", select: excludeFields })
+      .populate({ path: "participants", select: excludeFields });
+
+    resp.status(200).json({ message: "New conversation made", conversation });
+  } catch (err) {
+    next(err);
+  }
+};
+
+const updateConversation = async (req, resp, next) => {
+  const { id } = req.params;
+  const { body } = req;
+  try {
+    let updateConversation = await Conversation.findByIdAndUpdate(id, body, {
+      new: true,
+    })
+      .populate({ path: "manager", select: excludeFields })
+      .populate({ path: "participants", select: excludeFields });
+    resp
+      .status(200)
+      .json({ message: "Update", conversation: updateConversation });
+  } catch (err) {
+    next(err);
+  }
+};
+
+const addManager = async (req, resp, next) => {
+  const { conId } = req.params;
+  const { manager } = req.body;
+
+  try {
+    let updateConversation = await Conversation.findByIdAndUpdate(
+      conId,
+      { $push: { manager } },
+      { new: true }
+    )
+      .populate({ path: "manager", select: excludeFields })
+      .populate({ path: "participants", select: excludeFields });
+
+    return resp
+      .status(200)
+      .json({ message: "Manager added", conversation: updateConversation });
+  } catch (err) {
+    next(err);
+  }
+};
+
+const removeManager = async (req, resp, next) => {
+  const { conId } = req.params;
+  const { manager } = req.body;
+
+  try {
+    let updateConversation = await Conversation.findByIdAndUpdate(
+      conId,
+      { $pull: { manager } },
+      { new: true }
+    )
+      .populate({ path: "manager", select: excludeFields })
+      .populate({ path: "participants", select: excludeFields });
+
+    return resp
+      .status(200)
+      .json({ message: "Manager removed", conversation: updateConversation });
+  } catch (err) {
+    next(err);
+  }
+};
+
+const addMember = async (req, resp, next) => {
+  const { conId } = req.params;
+  const { participants } = req.body;
+
+  try {
+    let updateConversation = await Conversation.findByIdAndUpdate(
+      conId,
+      { $push: { participants } },
+      { new: true }
+    )
+      .populate({ path: "manager", select: excludeFields })
+      .populate({ path: "participants", select: excludeFields });
+
+    return resp
+      .status(200)
+      .json({ message: "Member added", conversation: updateConversation });
+  } catch (err) {
+    next(err);
+  }
+};
+
+const removeMember = async (req, resp, next) => {
+  const { conId } = req.params;
+  const { participants } = req.body;
+
+  try {
+    let updateConversation = await Conversation.findByIdAndUpdate(
+      { _id: conId },
+      { $pull: { participants, manager: participants } },
+      { new: true }
+    )
+      .populate({ path: "manager", select: excludeFields })
+      .populate({ path: "participants", select: excludeFields });
+
+    return resp
+      .status(200)
+      .json({ message: "Member removed", conversation: updateConversation });
+  } catch (err) {
+    next(err);
+  }
+};
+
+const deleteConversation = async (req, resp, next) => {
+  const { id } = req.params;
+  try {
+    let isDeleted = await Conversation.findByIdAndDelete(id);
+    if (isDeleted) {
+      await Message.deleteMany({ conversation: id });
+    }
+    resp.status(200).json("Conversation deleted!");
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = {
+  getAllConversations,
+  addNewConversation,
+  updateConversation,
+  addMember,
+  removeMember,
+  addManager,
+  removeManager,
+  deleteConversation,
+};
