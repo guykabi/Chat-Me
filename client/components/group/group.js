8f7552d006c5c5ae5a697db3b759c8f66bf35846
Guy Kabilio <guyka00@gmail.com>
2023-03-27 01:35:37 +0300

    joint group component

---
+++ client/components/group/group.js
@@ -0,0 +1,45 @@
+import React,{useContext} from 'react'
+import styles from './group.module.css'
+import noAvatarGroup from '../../public/images/no-avatarGroup.png'
+import Image from 'next/image'
+import { chatContext } from '../../context/chatContext'
+
+const Group = ({group,onPick}) => {
+   
+const {currentChat,currentUser,dispatch} = useContext(chatContext)
+
+
+const handlePick = (e) =>{
+  e.stopPropagation();
+  dispatch({type:'CURRENT_CHAT',payload:group})
+} 
+
+
+  return (
+    <>
+     <article className={styles.groupMainDiv} onClick={handlePick}>
+
+      <div className={styles.groupImageWrapper}>
+          <Image 
+           width={70}
+           height={70}
+           style={{borderRadius:'50%',objectFit:'cover'}}
+           src={group?.image?.url?group.image.url:noAvatarGroup}
+           alt={group.chatName}
+          />
+      </div>
+
+      <div className={styles.groupNameWrapper}>
+
+        <section>
+         {group?.chatName} 
+        </section>&nbsp;
+
+      </div>
+      
+     </article>
+    </>
+  )
+} 
+
+export default Group
