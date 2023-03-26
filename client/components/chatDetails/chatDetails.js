ee610ed71a522314fed065a4968957338660ccfc
Guy Kabilio <guyka00@gmail.com>
2023-03-27 01:59:32 +0300

    implementing files uploading + joint groups list

--- client/components/chatDetails/chatDetails.js
+++ client/components/chatDetails/chatDetails.js
@@ -1,14 +1,20 @@
-import React, { useContext, useState, useMemo, useEffect } from "react";
+import React, { useContext, useState, useMemo, useEffect, useRef } from "react";
 import styles from "./chatDetails.module.css";
+import Image from "next/image";
+import noAvatar from "../../public/images/no-avatar.png";
+import noAvatarGroup from "../../public/images/no-avatarGroup.png";
 import { chatContext } from "../../context/chatContext";
 import ReturnIcon from "../UI/returnIcon/returnIcon";
 import Input from "../UI/Input/Input";
 import GroupPerson from "../group-person/groupPerson";
+import Group from "../group/group";
 import Button from "../UI/Button/button";
-import { useMutation, useQuery } from "react-query";
+import { useMutation, useQuery, QueryClient } from "react-query";
 import Modal from "../Modal/modal";
 import PickedUser from "../pickedUser/pickedUser";
 import { BiSend } from "react-icons/bi";
+import { BsFillCameraFill } from "react-icons/bs";
+import { useGetFetchQuery } from "../../hooks/useGetQuery";
 import {
   updateConversation,
   getAllusers,
@@ -18,8 +24,10 @@ import {
   removeManager,
 } from "../../utils/apiUtils";
 
+
 const ChatDetails = ({ onReturn }) => {
-  const { currentChat,currentUser, Socket, dispatch } = useContext(chatContext);
+  const { currentChat, currentUser, Socket, dispatch } =
+    useContext(chatContext);
   const [showModal, setShowModal] = useState(false);
   const [allUsers, setAllUsers] = useState([]);
   const [isGroup, setIsGroup] = useState(false);
@@ -29,6 +37,10 @@ const ChatDetails = ({ onReturn }) => {
   const [isMenu, setIsMenu] = useState(null);
   const [errorText, setErrorText] = useState(null);
   const [addMemberErrorText, setAddMemberErrorText] = useState(null);
+  const conversations = useGetFetchQuery("conversations");
+  const fileRef = useRef(null);
+
+  ////////////API calls - reactQuery/////////////
 
   const { refetch } = useQuery("users", getAllusers, {
     onSuccess: (data) => {
@@ -98,7 +110,7 @@ const ChatDetails = ({ onReturn }) => {
       }, 3000);
       return () => clearTimeout(timer);
     },
-  }); 
+  });
 
   const { mutate: managerRemoval } = useMutation(removeManager, {
     onSuccess: ({ message, conversation }) => {
@@ -115,6 +127,8 @@ const ChatDetails = ({ onReturn }) => {
     },
   });
 
+  ////////////---------/////////////
+
   useEffect(() => {
     if (!currentChat.chatName) return;
     setIsGroup(true);
@@ -127,6 +141,12 @@ const ChatDetails = ({ onReturn }) => {
 
   const handleChatDetailChange = (e) => {
     const { value, name } = e.target;
+
+    if (e.target?.files?.[0]) {
+      const fileUploaded = e.target.files[0];
+      setGroupChangedDetails({ ...groupChangedDetails, [name]: fileUploaded });
+      return;
+    }
 
     if (value === currentChat.chatName && name === "chatName") {
       delete groupChangedDetails[name];
@@ -158,46 +178,59 @@ const ChatDetails = ({ onReturn }) => {
   const handleMemberRemoval = (e) => {
     let obj;
 
-    if(typeof e !== 'string' ){
-
-      //When the only manager wants to leave the group - 
+    if (typeof e !== "string") {
+      //When the only manager wants to leave the group -
       //Sets a random member of the group as manager
-      if(currentChat.manager.length === 1 && 
-         currentChat.manager[0]._id === currentUser._id)
-         {
-          
-         let setAsManager =  currentChat.participants.find(p=>p._id !== currentUser._id)
-         let obj = { manager: setAsManager._id };
-         setManager({ conId: currentChat._id, obj });
-         }
+      if (
+        currentChat.manager.length === 1 &&
+        currentChat.manager[0]._id === currentUser._id
+      ) {
+        let setAsManager = currentChat.participants.find(
+          (p) => p._id !== currentUser._id
+        );
+        let obj = { manager: setAsManager._id };
+        setManager({ conId: currentChat._id, obj });
+      }
 
-      let obj = {participants:currentUser._id}
+      let obj = { participants: currentUser._id };
       removeMember({ conId: currentChat._id, obj });
-     return
-
+      return;
     }
 
-     obj = {participants: e}
-     removeMember({ conId: currentChat._id, obj });
-  }; 
+    obj = { participants: e };
+    removeMember({ conId: currentChat._id, obj });
+  };
 
   const handleManagerAdding = (e) => {
     let obj = { manager: e };
     setManager({ conId: currentChat._id, obj });
-  };  
+  };
 
-  const handleManagerRemoval = (e) =>{
+  const handleManagerRemoval = (e) => {
     let obj = { manager: e };
     managerRemoval({ conId: currentChat._id, obj });
-  }
+  };
 
   const submitChatDetailChange = (e) => {
     e.preventDefault();
     if (!Object.keys(groupChangedDetails).length) return;
-    update({ conId: currentChat._id, obj: groupChangedDetails });
+    const data = new FormData();
+    for (const [key, value] of Object.entries(groupChangedDetails)) {
+      data.append([key], value);
+    }
+
+    if (currentChat?.image?.url) {
+      data.append("removeImage", currentChat.image.cloudinary_id);
+    }
+
+    update({ conId: currentChat._id, obj: data });
   };
 
-  const memoMembers = useMemo(
+  const handleInputFileClick = (e) => {
+    fileRef.current.click();
+  };
+
+  const memoGroupMembers = useMemo(
     () =>
       currentChat.participants.map((user) => (
         <GroupPerson
@@ -208,10 +241,23 @@ const ChatDetails = ({ onReturn }) => {
           onRemoveManager={handleManagerRemoval}
           onMenu={(e) => setIsMenu(e)}
           menu={isMenu}
-          manager={currentChat?.manager.some(m=>m._id === user._id)}
+          manager={currentChat?.manager.some((m) => m._id === user._id)}
         />
       )),
     [isMenu]
+  );
+
+  const jointGroups = useMemo(
+    () =>
+      conversations
+        .filter(
+          (j) =>
+            j.chatName &&
+            j.participants.some((p) => p._id === currentChat?.friend?._id)
+        )
+        .map((con) => <Group key={con._id} group={con} />),
+
+    [conversations]
   );
 
   const allUsersToPick = useMemo(
@@ -269,21 +315,51 @@ const ChatDetails = ({ onReturn }) => {
           {errorText && <p>{errorText}</p>}
           <article className={styles.chatImageWrapper}>
             {isGroup ? (
-              <img
-                src={
-                  currentChat?.image
-                    ? currentChat.image
-                    : "/images/no-avatarGroup.png"
-                }
-                alt={currentChat.chatName}
-              />
+              <>
+                <div>
+                  <Image
+                    src={
+                      currentChat?.image?.url
+                        ? currentChat.image.url
+                        : noAvatarGroup
+                    }
+                    width={150}
+                    height={140}
+                    style={{
+                      objectFit: "cover",
+                      borderRadius: "50%",
+                      marginTop: "10%",
+                    }}
+                    alt={currentChat.chatName}
+                  />
+                </div>
+                <div className={styles.chooseImageWrapper}>
+                  <BsFillCameraFill onClick={handleInputFileClick} />
+                  <Input
+                    type="file"
+                    name="userImage"
+                    onChange={handleChatDetailChange}
+                    className="invisibleFileInput"
+                    width={0}
+                    height={0}
+                    ref={fileRef}
+                  />
+                </div>
+              </>
             ) : (
-              <img
+              <Image
+                width={150}
+                height={120}
                 src={
-                  currentChat?.friend?.image
-                    ? currentChat.friend.image
-                    : "/images/no-avatar.png"
+                  currentChat?.friend?.image?.url
+                    ? currentChat.friend.image.url
+                    : noAvatar
                 }
+                style={{
+                  objectFit: "cover",
+                  borderRadius: "50%",
+                  marginTop: "10%",
+                }}
                 alt={currentChat?.friend?.name}
               />
             )}
@@ -291,9 +367,17 @@ const ChatDetails = ({ onReturn }) => {
         </form>
       </header>
 
-      <section className={styles.membersOfChat}>
-        {isGroup && memoMembers}
-      </section>
+      {!isGroup && (
+        <h2>
+          {`Joint Groups with ${currentChat?.friend?.name} - (${jointGroups.length})`}
+        </h2>
+      )}
+
+      {isGroup ? (
+        <section className={styles.groupMembers}>{memoGroupMembers}</section>
+      ) : (
+        <section className={styles.jointGroupsWrapper}>{jointGroups}</section>
+      )}
 
       {isGroup && (
         <section className={styles.buttonsWrapper}>
@@ -307,9 +391,11 @@ const ChatDetails = ({ onReturn }) => {
           <Button
             width="8"
             height="25"
-            text="Add member" 
+            text="Add member"
             //Only manager can add
-            disabled={!currentChat?.manager.some(m=>m._id === currentUser._id)}
+            disabled={
+              !currentChat?.manager.some((m) => m._id === currentUser._id)
+            }
             className="secondaryBtn"
             onClick={handleOpenModal}
           />
