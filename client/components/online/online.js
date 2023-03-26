45c65d72ae91a96362131eebc921de30f1648a1b
Guy Kabilio <guyka00@gmail.com>
2023-03-27 02:21:05 +0300

    adding next-image + fixing image round border

--- client/components/online/online.js
+++ client/components/online/online.js
@@ -1,16 +1,26 @@
-import React,{memo} from 'react'
-import styles from './online.module.css'
+import React, { memo } from "react";
+import styles from "./online.module.css";
+import noAvatar from "../../public/images/no-avatar.png";
+import Image from "next/image";
 
-const Online = ({user}) => {
+const Online = ({ user }) => {
   return (
-    <div className={styles.mainOnlineDiv}>
-        <div className={styles.chatOnlineImgContainer}>
-            <img className={styles.chatOnlineImg} src='/images/no-avatar.png' />
-            <div className={styles.chatOnlineBadge}></div>
-        </div>
-        <span className={styles.chatOnlineName}>{user.name}</span>
+    <>
+    <div className={styles.mainOnlineDiv} aria-label="online user">
+      <div className={styles.chatOnlineImgContainer}>
+        <Image
+          style={{borderRadius:'50%'}}
+          src={user?.image?.url ? user.image.url : noAvatar}
+          alt={user.name}
+          width={35}
+          height={35}
+        />
+        <div className={styles.chatOnlineBadge}></div>
+      </div>
+      <span className={styles.chatOnlineName}>{user.name}</span>
     </div>
-  )
-}
+    </>
+  );
+};
 
-export default memo(Online)
+export default memo(Online);
