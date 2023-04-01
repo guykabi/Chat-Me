c000a076b10c8f31cd142dfd6030e318fe0fa8cb
Guy Kabilio <guyka00@gmail.com>
2023-04-01 23:02:11 +0300

    adding like & delete message routs

--- Server/routes/messageRouter.js
+++ Server/routes/messageRouter.js
@@ -6,12 +6,16 @@ const upload = require('../middleware/upload')
 const {
      getMessageByConId,
      addNewMessage,
-     handleSeenMessage} = require('../controllers/messageController') 
+     handleSeenMessage,
+     likeMessage,
+     deleteMessage} = require('../controllers/messageController') 
 
 router
 .get('/:conversation',Auth,getMessageByConId)  
 .post('/',Auth,upload.single('messageImage'),addNewMessage)
 .patch('/seen/:id',handleSeenMessage)
+.patch('/like-message/:id',Auth,likeMessage)
+.delete('/delete-message/:id',Auth,deleteMessage)
 
 
 module.exports = router;
