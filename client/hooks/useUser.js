ca9385a4a26bc428ca533630d5b17366b2fb3976
Guy Kabilio <guyka00@gmail.com>
2023-04-06 11:55:31 +0300

    wrapping query key name

--- client/hooks/useUser.js
+++ client/hooks/useUser.js
@@ -2,7 +2,7 @@ import { useQuery } from "react-query"
 import { getUserDetails } from "../utils/apiUtils"
 
 export const useGetUser = (userId,enabled=true,onSuccess,onError)=>{
-  return useQuery('user',()=>getUserDetails(userId),
+  return useQuery(['user'],()=>getUserDetails(userId),
   { 
     onSuccess,
     onError,
