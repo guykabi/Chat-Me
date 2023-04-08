c0ba178c96f21f73ad280be3a25a21dcd8d0f9ea
Guy Kabilio <guyka00@gmail.com>
2023-04-08 19:50:21 +0300

    implementing global picker component

--- client/components/message/message.js
+++ client/components/message/message.js
@@ -8,10 +8,10 @@ import React, {
 } from "react";
 import { chatContext } from "../../context/chatContext";
 import styles from "./message.module.css";
-import { getTime, searchPastMember } from "../../utils/utils";
-import useClickOutSide from '../../hooks/useClickOutside'
+import { getTime, searchPastMember, handleFilterCons } from "../../utils/utils";
+import useClickOutSide from "../../hooks/useClickOutside";
 import { useMutation } from "react-query";
-import { seenMessage } from "../../utils/apiUtils";
+import { seenMessage, handleForwardMessage } from "../../utils/apiUtils";
 import MessageOperations from "./messageOperation/messageOperations";
 import { FcLike } from "react-icons/fc";
 import { AiOutlineDown } from "react-icons/ai";
@@ -19,22 +19,32 @@ import Image from "next/image";
 import Modal from "../Modal/modal";
 import GroupPerson from "../group-person/groupPerson";
 import { useGetCacheQuery } from "../../hooks/useGetQuery";
+import Picker from "../picker/picker";
 
 const Message = forwardRef(({ message, own }, ref) => {
-  const { currentChat, currentUser } = useContext(chatContext);
+  const { currentChat, currentUser, Socket } = useContext(chatContext);
   const [memeberName, setMemberName] = useState(null);
   const [showModal, setShowModal] = useState(false);
   const [showImage, setShowImage] = useState(false);
+  const [showForward, setShowForward] = useState(false);
   const [toggleDetailsMenu, setToggleDetailsMenu] = useState(true);
-  //To controlling the outSide clicking - global hook
   const { visibleRef, isVisible, setIsVisible } = useClickOutSide(false);
   const allUsers = useGetCacheQuery("users");
+  const conversations = useGetCacheQuery("conversations");
 
   const { mutate: switchToSeen } = useMutation(seenMessage);
-  
+
+  const { mutate: forward } = useMutation(handleForwardMessage, {
+    onSuccess: (data) => {
+      if (data.message !== "New message just added") return;
+      setShowForward(false);
+      Socket.emit("forward-message", data.data, data.receivers);
+    },
+  });
+
   useEffect(() => {
     //Checks if message is unseen
-    if(message?.banner)return
+    if (message?.banner) return;
     if (!own && !message.seen.some((s) => s.user === currentUser._id)) {
       switchToSeen({ messageId: message._id, userId: currentUser._id });
     }
@@ -52,7 +62,7 @@ const Message = forwardRef(({ message, own }, ref) => {
 
   const openModal = useCallback(() => {
     setShowModal(true);
-  },[showModal]);
+  }, [showModal]);
 
   const closeModal = () => {
     setToggleDetailsMenu(true);
@@ -64,7 +74,25 @@ const Message = forwardRef(({ message, own }, ref) => {
     setShowImage(true);
   };
 
-  
+  const openForwardModal = () => {
+    setShowForward(true);
+  };
+
+  const closeForward = () => {
+    setShowForward(false);
+  };
+
+  const handleForward = (e) => {
+    let fixedMessage = {};
+    fixedMessage.text = message.text;
+    fixedMessage.sender = currentUser._id;
+    if (message?.image) {
+      fixedMessage.image = message.image;
+    }
+
+    forward({ receivers: e, fixedMessage });
+  };
+
   const messageDetails = (
     <section className={styles.messageDetailsWrapper}>
       <header className={styles.messageDetailsHeader}>
@@ -126,82 +154,96 @@ const Message = forwardRef(({ message, own }, ref) => {
 
   return (
     <>
-      {!message.banner?
-      <article className={styles.mainMessageDiv} ref={ref}>
-        <main
-          className={styles.contentWrapper}
-          ref={visibleRef}
-        >
-          <div className={own ? styles.ownMessage : styles.otherMessage}>
-            {currentChat.chatName && (
-              <header className={styles.topOfMessage}>
-                <div aria-label={memeberName}>
-                  <strong>{memeberName}</strong>
-                </div>
-                {message?.image ? 
+      {!message.banner ? (
+        <article className={styles.mainMessageDiv} ref={ref}>
+          <main className={styles.contentWrapper} ref={visibleRef}>
+            <div className={own ? styles.ownMessage : styles.otherMessage}>
+              {currentChat.chatName && (
+                <header className={styles.topOfMessage}>
+                  <div aria-label={memeberName}>
+                    <strong>{memeberName}</strong>
+                  </div>
+                  {message?.image ? (
+                    <div
+                      className={styles.dropMenuSignImage}
+                      onClick={handleMessageOperationMenu}
+                    >
+                      <AiOutlineDown />
+                    </div>
+                  ) : (
+                    <div
+                      className={styles.dropMenuSign}
+                      onClick={handleMessageOperationMenu}
+                    >
+                      <AiOutlineDown />
+                    </div>
+                  )}
+                </header>
+              )}
+
+              {!currentChat.chatName && (
+                <header className={styles.topOfMessage}>
                   <div
-                    className={styles.dropMenuSignImage}
+                    className={styles.dropMenuSign}
                     onClick={handleMessageOperationMenu}
                   >
                     <AiOutlineDown />
-                  </div>:
-                  <div
-                  className={styles.dropMenuSign}
-                  onClick={handleMessageOperationMenu}
-                >
-                  <AiOutlineDown />
+                  </div>
+                </header>
+              )}
+              {message.image ? (
+                <div className={styles.messageImageWrapper} onClick={openImage}>
+                  {image}
                 </div>
-                }
-              </header>
-            )}
-
-            {!currentChat.chatName&&
-            <header className={styles.topOfMessage}>
-                <div
-                className={styles.dropMenuSign}
-                onClick={handleMessageOperationMenu}
-                >
-                <AiOutlineDown />
+              ) : null}
+              {message.text && (
+                <div dir="auto" className={styles.messageTextDiv}>
+                  {message.text}
                 </div>
-            </header>}
-            {message.image ? (
-              <div className={styles.messageImageWrapper} onClick={openImage}>
-                {image}
-              </div>
-            ) : null}
-            {message.text && (
-              <div dir="auto" className={styles.messageTextDiv}>
-                {message.text}
-              </div>
-            )}
+              )}
 
-            <div className={styles.timeBatch}>{getTime(message.createdAt)}</div>
-
-            {message?.likes?.length ? (
-              <div className={styles.likedMessageSign}>
-                <FcLike />
-                {message.likes.length > 1 ? (
-                  <span className={styles.numsOfLikes}>
-                    {message.likes.length}
-                  </span>
-                ) : null}
+              <div className={styles.timeBatch}>
+                {getTime(message.createdAt)}
               </div>
-            ) : null}
 
-            {isVisible && (
-              <MessageOperations
-                messageId={message._id}
-                own={own}
-                onCloseMenu={()=>setIsVisible(false)}
-                ownLike={message.likes.includes(currentUser._id)}
-                onDetailModal={openModal}
-              />
-            )}
-          </div>
-        </main>
-        <Modal show={showModal} onClose={closeModal} title="Message Details">
-          {messageDetails}
-        </Modal>
+              {message?.likes?.length ? (
+                <div className={styles.likedMessageSign}>
+                  <FcLike />
+                  {message.likes.length > 1 ? (
+                    <span className={styles.numsOfLikes}>
+                      {message.likes.length}
+                    </span>
+                  ) : null}
+                </div>
+              ) : null}
+
+              {isVisible && (
+                <MessageOperations
+                  messageId={message._id}
+                  own={own}
+                  onCloseMenu={() => setIsVisible(false)}
+                  ownLike={message.likes.includes(currentUser._id)}
+                  onDetailModal={openModal}
+                  onForwardModal={openForwardModal}
+                />
+              )}
+            </div>
+          </main>
+          <Modal show={showModal} onClose={closeModal} title="Message Details">
+            {messageDetails}
+          </Modal>
+
+          <Modal
+            show={showForward}
+            onClose={closeForward}
+            title="Forward to..."
+          >
+            <Picker
+              items={conversations}
+              type="cons"
+              onFinalPick={handleForward}
+            />
+          </Modal>
 
           <Modal
             show={showImage}
@@ -210,12 +252,12 @@ const Message = forwardRef(({ message, own }, ref) => {
           >
             <div className={styles.modalImageMessage}>{image}</div>
           </Modal>
-      </article>:
-      <article className={styles.bannerUnSeen}>
-        <div className={styles.bannerContentWrapper}>
-        {message.banner}
-        </div>
-      </article>}
+        </article>
+      ) : (
+        <article className={styles.bannerUnSeen}>
+          <div className={styles.bannerContentWrapper}>{message.banner}</div>
+        </article>
+      )}
     </>
   );
 });
