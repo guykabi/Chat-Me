ff56625d5084ec20bcd74c678740866a7566b5db
Guy Kabilio <guyka00@gmail.com>
2023-04-08 19:36:47 +0300

    implementing global picker component

--- client/components/createGroup/createGroup.js
+++ client/components/createGroup/createGroup.js
@@ -1,47 +1,37 @@
-import React, { useState, useContext, useMemo, useCallback, useEffect } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import styles from "./createGroup.module.css";
 import { useMutation, useQuery } from "react-query";
 import { createGroup } from "../../utils/apiUtils";
 import { chatContext } from "../../context/chatContext";
-import GroupPerson from "../group-person/groupPerson";
 import Button from "../UI/Button/button";
-import PickedUser from "../pickedUser/pickedUser";
-import {useGetCacheQuery} from '../../hooks/useGetQuery'
+import { useGetCacheQuery } from "../../hooks/useGetQuery";
+import Picker from "../picker/picker";
 
 const CreateGroup = ({ onSwitch }) => {
   const { currentUser, Socket } = useContext(chatContext);
   const [allUsers, setAllUsers] = useState([]);
-  const [query, setQuery] = useState("");
   const [groupName, setGroupName] = useState(null);
   const [pickedUsers, setPickedUsers] = useState([]);
-  const users = useGetCacheQuery('users')
+  const users = useGetCacheQuery("users");
 
-  
   const { mutate: addGroup } = useMutation(createGroup, {
     onSuccess: (data) => {
-      if(data.message !== 'New conversation made')return
+      if (data.message !== "New conversation made") return;
       setGroupName(null);
       setPickedUsers([]);
       Socket.emit("new-conversation", data.conversation);
       onSwitch();
     },
-  }); 
+  });
 
-  useEffect(()=>{
-    let filteredUsers = users.
-    filter((u) => u._id !== currentUser._id);
+  useEffect(() => {
+    let filteredUsers = users.filter((u) => u._id !== currentUser._id);
     setAllUsers(filteredUsers);
-},[])
+  }, []);
 
-  const handleUserPick = useCallback((e) => {
-    setQuery("");
-    if (pickedUsers.find((p) => p._id === e._id)) return;
-    setPickedUsers((prev) => [...prev, e]);
-  },[pickedUsers]);
-
-  const removePickedUser =useCallback((pickedUser) => {
-    setPickedUsers((prev) => prev.filter((u) => u._id !== pickedUser._id));
-  },[pickedUsers]);
+  const addPickedUsers = (e) => {
+    setPickedUsers(e);
+  };
 
   const handleGroupSubmit = (e) => {
     e.preventDefault();
@@ -49,28 +39,12 @@ const CreateGroup = ({ onSwitch }) => {
     if (!pickedUsers.length) return;
     let group = {};
     group.chatName = groupName;
-    group.participants = pickedUsers.map((p) => p._id);
+    group.participants = pickedUsers;
     group.participants.unshift(currentUser._id);
     group.manager = currentUser._id;
 
     addGroup(group);
   };
-
-
-
-  const filteredItems = useMemo(() => {
-    return allUsers?.filter((item) =>
-      item.name?.toLowerCase().includes(query?.toLowerCase())
-    );
-  }, [allUsers, query]);
-
-  const searchForUsers = filteredItems?.map((user) => (
-    <GroupPerson key={user._id} user={user} onPick={handleUserPick} />
-  ));
-
-  const pickedUsersForGroup = pickedUsers.map((user) => (
-    <PickedUser key={user._id} user={user} onRemove={removePickedUser} />
-  ));
 
   return (
     <section className={styles.createGroupMainSection}>
@@ -86,30 +60,9 @@ const CreateGroup = ({ onSwitch }) => {
             />
           </div>
           <br />
-
-          {pickedUsersForGroup.length ? (
-            <div
-              className={
-                pickedUsers.length
-                  ? styles.addedFriendsListActive
-                  : styles.addedFriendsList
-              }
-            >
-              {pickedUsersForGroup}
-            </div>
-          ) : null}
-
-          <article className={styles.searchInputWrapper}>
-            <input
-              value={query}
-              className={styles.searchFriendToAddInput}
-              placeholder="Search user to add"
-              aria-label="Search user to add"
-              onChange={(e) => setQuery(e.target.value)}
-            />
-          </article>
-
-          <article className={styles.allUsers}>{searchForUsers}</article>
+          <section className={styles.pickerWrapper}>
+            <Picker items={allUsers} type="users" onChange={addPickedUsers} />
+          </section>
           <br />
           <br />
 
