68853be24befc99a2aff8444aaaabdf499650593
Guy Kabilio <guyka00@gmail.com>
2023-04-08 19:48:55 +0300

    adding condition for non group

--- client/components/group/group.js
+++ client/components/group/group.js
@@ -1,38 +1,55 @@
 import React,{useContext,memo} from 'react'
 import styles from './group.module.css'
 import noAvatarGroup from '../../public/images/no-avatarGroup.png'
+import noAvatar from '../../public/images/no-avatar.png'
 import Image from 'next/image'
 import { chatContext } from '../../context/chatContext'
 
-const Group = ({group}) => {
+const Group = ({group,onPick}) => {
    
-const {dispatch} = useContext(chatContext)
-
+const {dispatch,currentUser} = useContext(chatContext)
 
 const handlePick = (e) =>{
   e.stopPropagation();
   dispatch({type:'CURRENT_CHAT',payload:group})
 } 
 
+const onGroupPick = (e) =>{
+  e.stopPropagation()
+  onPick(group)
+}
+
+const getUserName = ()=> {
+  let user = group.participants.find(p=>p._id !== currentUser._id)
+  return user.name
+}
 
   return (
     <>
-     <article className={styles.groupMainDiv} onClick={handlePick}>
+     <article className={styles.groupMainDiv} onClick={onPick?onGroupPick:handlePick}>
 
       <div className={styles.groupImageWrapper}>
+          {group?.chatName?
           <Image 
            width={70}
            height={70}
            style={{borderRadius:'50%',objectFit:'cover'}}
            src={group?.image?.url?group.image.url:noAvatarGroup}
            alt={group.chatName}
-          />
+          />:
+          <Image
+           width={70}
+           height={70}
+           style={{borderRadius:'50%',objectFit:'cover'}}
+           src={group?.image?.url?group.image.url:noAvatar}
+           alt={getUserName()}
+          />}
       </div>
 
       <div className={styles.groupNameWrapper}>
 
         <section>
-         {group?.chatName} 
+         {group?.chatName?group.chatName:getUserName()} 
         </section>&nbsp;
 
       </div>
