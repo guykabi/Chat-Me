9c8a1ff56766cd85beb966a27d9a5a20a06fa0be
Guy Kabilio <guyka00@gmail.com>
2023-04-12 16:31:04 +0300

    adding logic for retaining all connected users after redirect to messenger page

--- client/components/onlineList/online.js
+++ client/components/onlineList/online.js
@@ -6,16 +6,17 @@ import { chatContext } from '../../context/chatContext'
 
 const OnlineList = () => {  
 
-  const [onlineUsers,setOnlineUsers]=useState()
+  const [onlineUsers,setOnlineUsers]=useState([])
   const {currentUser,Socket} = useContext(chatContext)
 
 
  useEffect(()=>{
   
     Socket.removeAllListeners('getUsers')
-
+    
     const handleGetUsersEvent = (users) =>{
       
+    
       let allOnLineUsers = users.filter(user=>user.userId !== currentUser._id)
       .map(u=>u.userId)
  
@@ -31,8 +32,8 @@ const OnlineList = () => {
    //When a new friend added - add him (if he is connect) to the online list!
    const handleNotifyEvent = ({users,message}) =>{
   
-    if(message !== 'The Friend approval has been done')return
-     
+    if(message !== 'The Friend approval has been done')return 
+    
     let allOnLinUsers = users?.map(u=>u._id)
     let onlineFriends =  currentUser.friends
     ?.filter(f=> allOnLinUsers.includes(f._id))
@@ -45,6 +46,12 @@ const OnlineList = () => {
   
  },[Socket])
 
+
+ //When messanger page reload for the first time (user redirected to the page)
+ if(!onlineUsers.length){     
+        Socket.emit('all-connected')
+        Socket.removeAllListeners('all-connected')
+ }
 
  useEffect(()=>{
   //When there is a change on the user's friends
