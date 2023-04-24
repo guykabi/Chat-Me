acce813f0850069dbe53224139142a3f2fe5d328
Guy Kabilio <guyka00@gmail.com>
2023-04-24 18:46:17 +0300

    adding staleTime

--- client/hooks/useGetQuery.js
+++ client/hooks/useGetQuery.js
@@ -2,6 +2,9 @@ import { useQuery } from "react-query";
 
 
 export const useGetCacheQuery = (key) => {
-  const { data } = useQuery(key);
+  
+  const { data } = useQuery(key,{
+    staleTime:100000
+  });
   return data;
 };
