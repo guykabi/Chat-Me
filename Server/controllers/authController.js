c14ba337d439b7f11aa1d324422172cdba7b740a
Guy Kabilio <guyka00@gmail.com>
2023-04-30 04:47:48 +0300

    adding a logic for validate the reset password link

--- Server/controllers/authController.js
+++ Server/controllers/authController.js
@@ -1,21 +1,7 @@
 import {User} from '../models/messagesModel.js'
 import {generateInitialToken} from '../utils/utils.js'
+import {verify} from 'jsonwebtoken'
 
-
-export const logOut = async(req,resp)=>{
-    resp.cookie('token', 'none', {
-        expires: new Date(Date.now() + 5 * 1000),
-        httpOnly: true,
-    }) 
-    resp.cookie('userData', 'none', {
-        expires: new Date(Date.now() + 5 * 1000),
-        httpOnly: true,
-    })
-    resp
-    .status(200)
-    .json({ success: true, message: 'User logged out successfully' })
-  }
-  
   
   export const checkValidity = async(req,resp,next)=>{
     try{
@@ -56,4 +42,39 @@ export const logOut = async(req,resp)=>{
      } catch(err){next(err)}
   }
   
+
+  export const validateResetLink =async (req,resp,next)=>{
+    
+      const {id,token} = req.body 
+      try{
+           let user = await User.findById(id)
+           if(!user)return resp.status(200).json('Not such user')
+            
+           verify(token,process.env.RESET_TOKEN, async (err)=>{
+               if(err){
+                 return next(new Error('Invalid token from reset link'))
+               }
+               return resp.status(200).json('Valid reset password token')
+           })
+
+
+      }catch(error){
+        next(error)
+      }
+  }
+
+  
+  export const logOut = async(req,resp)=>{
+    resp.cookie('token', 'none', {
+        expires: new Date(Date.now() + 5 * 1000),
+        httpOnly: true,
+    }) 
+    resp.cookie('userData', 'none', {
+        expires: new Date(Date.now() + 5 * 1000),
+        httpOnly: true,
+    })
+    resp
+    .status(200)
+    .json({ success: true, message: 'User logged out successfully' })
+  }
   
