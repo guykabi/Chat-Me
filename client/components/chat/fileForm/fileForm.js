417174ebf2c7ca939fae3b569d1c8d153cf083f4
Guy Kabilio <guyka00@gmail.com>
2023-05-03 11:26:40 +0300

    adding selected video preview

--- client/components/chat/fileForm/fileForm.js
+++ client/components/chat/fileForm/fileForm.js
@@ -1,58 +1,70 @@
-import React,{useContext,useEffect,useState} from 'react'
-import styles from './fileForm.module.css'
-import { chatContext } from '../../../context/chatContext'
-import Image from 'next/image'
-import Button from '../../UI/Button/button'
+import React, { useContext, useEffect, useState } from "react";
+import styles from "./fileForm.module.css";
+import { chatContext } from "../../../context/chatContext";
+import Image from "next/image";
+import Button from "../../UI/Button/button";
 import InputEmoji from "react-input-emoji";
+import Video from "../../UI/video/video";
 
+const FileForm = ({ file, onFile, loading }) => {
+  const { currentChat, currentUser } = useContext(chatContext);
+  const [fileMessage, setFileMessage] = useState("");
+  const [preview, setPreview] = useState(null);
+  const [fileType, setFileType] = useState(null);
 
-const FileForm = ({file,onFile,loading}) => {
+  useEffect(() => {
+    let pre = URL.createObjectURL(file);
+    setPreview(pre);
+    setFileType(file.type.split("/")[0]);
+  }, []);
 
-const {currentChat,currentUser} = useContext(chatContext)
-const [fileMessage,setFileMessage]=useState('')
-const [preview,setPreview]=useState(null)
+  const handleFileForm = (e) => {
+    e.preventDefault();
+    let messageObj = {};
+    messageObj.conversation = currentChat._id;
+    messageObj.sender = currentUser._id;
+    messageObj.text = fileMessage && fileMessage;
 
-useEffect(()=>{
-    let pre = URL.createObjectURL(file)
-    setPreview(pre)
-},[])
-
-const handleFileForm = (e) =>{
-   e.preventDefault()
-   let messageObj = {} 
-   messageObj.conversation = currentChat._id
-   messageObj.sender = currentUser._id
-   messageObj.text = fileMessage&&fileMessage
-   
-   if(file){
-    const data = new FormData();
-     for (const [key, value] of Object.entries(messageObj)) {
-       data.append([key], value);
-     }
-    data.append('messageImage',file)
-    onFile({message:'File',data})
-   } 
-}
+    if (file) {
+      const data = new FormData();
+      for (const [key, value] of Object.entries(messageObj)) {
+        data.append([key], value);
+      }
+      data.append("messageImage", file);
+      onFile({ message: "File", data });
+    }
+  };
 
   return (
     <>
-    <section className={styles.fileFormWrapper}>
+      <section className={styles.fileFormWrapper}>
         <header>
-           <h3>Your Image</h3>
+          {fileType == "image" ? <h3>Your Image</h3> : <h3>Your Video</h3>}
         </header>
         <main className={styles.formWrapper}>
-        <form onSubmit={handleFileForm} className={styles.form}>
-        <div 
-        aria-label='image preview'
-        className={styles.previewImageWrapper}>
-               {preview&&<Image
-               fill
-               style={{objectFit:'cover'}}
-               src={preview}
-               alt='chosen'/>}
+          <form onSubmit={handleFileForm} className={styles.form}>
+            <div
+              aria-label="file preview"
+              className={styles.previewImageWrapper}
+            >
+              {preview &&
+                (fileType == "image" ? (
+                  <>
+                    <Image
+                      fill
+                      style={{ objectFit: "cover" }}
+                      src={preview}
+                      alt="chosen"
+                    />
+                  </>
+                ) : (
+                  <>
+                    <Video video={URL.createObjectURL(file)} preview={true} />
+                  </>
+                ))}
             </div>
             <div className={styles.imageTextInput}>
-               <InputEmoji
+              <InputEmoji
                 value={fileMessage}
                 onChange={setFileMessage}
                 width={60}
@@ -62,19 +74,19 @@ const handleFileForm = (e) =>{
               />
             </div>
             <div className={styles.submitBtnWrapper}>
-            <Button
-            className='secondaryBtn'
-            text={loading?'Loading...':'Send'}
-            type='submit'
-            width={8}
-            height={10}
-            />
+              <Button
+                className="secondaryBtn"
+                text={loading ? "Loading..." : "Send"}
+                type="submit"
+                width={8}
+                height={10}
+              />
             </div>
-            </form>
+          </form>
         </main>
-    </section>
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default FileForm
+export default FileForm;
