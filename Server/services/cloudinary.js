84ed4b1edff350ab118a82497c6617c2384caa29
Guy Kabilio <guyka00@gmail.com>
2023-05-03 11:18:04 +0300

    condition for videos files + video files over 60MB(upload_large)

--- Server/services/cloudinary.js
+++ Server/services/cloudinary.js
@@ -1,28 +1,37 @@
-import cloudinary from 'cloudinary'
+import cloudinary from "cloudinary";
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET
-}) 
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
-
-export const uploadToCloudinary = async(path,folder,next) =>{
-  try{
-    
-    const data = await cloudinary.v2.uploader.upload(path,{folder})    
-    return {url:data.url ,cloudinary_id:data.public_id}
-  }catch(err){
-    next(err)
+export const uploadToCloudinary = async (file, folder, next) => {
+  try {
+    if (file.mimetype.includes("video")) {
+      if (file.size / (1024 * 1024) >= 60) {
+        //Heavy video - over 60MB
+        const data = await cloudinary.v2.uploader.upload_large(file.path, {
+          resource_type: "video",
+          chunk_size: 10000000,
+        });
+        return { url: data.url, cloudinary_id: data.public_id, video: true };
+      }
+      const data = await cloudinary.v2.uploader.upload(file.path, {
+        resource_type: "video",
+        folder,
+      });
+      return { url: data.url, cloudinary_id: data.public_id, video: true };
+    }
+    const data = await cloudinary.v2.uploader.upload(file.path, { folder });
+    return { url: data.url, cloudinary_id: data.public_id };
+  } catch (err) {
+    next(err);
   }
-       
-}  
+};
 
-
-export const removeFromCloudinary =async (public_id) =>{
-    await cloudinary.v2.uploader.destroy(public_id, (error,result)=>{
-        return 'Removed successfully'
-    })
-}
-
-
+export const removeFromCloudinary = async (public_id) => {
+  await cloudinary.v2.uploader.destroy(public_id, (error, result) => {
+    return "Removed successfully";
+  });
+};
