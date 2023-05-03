07b14988c786dfa7aee9dfe0be8b4b512f64735f
Guy Kabilio <guyka00@gmail.com>
2023-05-03 11:12:51 +0300

    filter for unsupported files types

--- Server/middleware/upload.js
+++ Server/middleware/upload.js
@@ -2,6 +2,18 @@ import multer from 'multer'
 
 const storage = multer.diskStorage({})
 
-export const upload = multer({storage})
+export const upload = multer({
+    storage,
+    fileFilter: (req, file, cb) => {
+
+    const acceptedTypes = file.mimetype.split('/');
+    if (acceptedTypes[0] === 'image' || acceptedTypes[0] === 'video') {
+          cb(null, true);
+        } else {
+          cb(null, false);
+          return cb(new Error('Only image or video allowed!'));
+        }
+      }
+})
 
 
