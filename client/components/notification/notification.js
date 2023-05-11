ec1c10b40031c0dd084d80659254bb2439ff84b3
Guy Kabilio <guyka00@gmail.com>
2023-05-11 19:59:26 +0300

    changing style + elements position + prettier

--- client/components/notification/notification.js
+++ client/components/notification/notification.js
@@ -1,110 +1,112 @@
-import React,{useContext, useEffect, useState} from 'react'
-import styles from './notification.module.css'
-import Image from 'next/image'
-import noAvatar from '../../public/images/no-avatar.png'
-import { chatContext } from '../../context/chatContext'
-import { useMutation } from 'react-query'
-import {excludeFieldsUserData} from '../../utils/utils'
-import { approveFriend , unapproveFriend } from '../../utils/apiUtils'
-import {useErrorBoundary} from 'react-error-boundary'
+import React, { useContext, useEffect, useState } from "react";
+import styles from "./notification.module.css";
+import Image from "next/image";
+import noAvatar from "../../public/images/no-avatar.png";
+import { chatContext } from "../../context/chatContext";
+import { useMutation } from "react-query";
+import { excludeFieldsUserData } from "../../utils/utils";
+import { approveFriend, unapproveFriend } from "../../utils/apiUtils";
+import { useErrorBoundary } from "react-error-boundary";
 
+const Notification = ({ notification, decreaseNotify }) => {
+  const { currentUser, dispatch, Socket } = useContext(chatContext);
+  const { showBoundary } = useErrorBoundary();
+  const [isRequest, setIsRequest] = useState(true);
 
-const Notification = ({notification,decreaseNotify}) => {
- const {currentUser,dispatch,Socket} = useContext(chatContext)
- const {showBoundary} = useErrorBoundary()
- const [isRequest,setIsRequest]=useState(true)
+  const { mutate: approveRequest } = useMutation(approveFriend, {
+    onSuccess: ({ message, user }) => {
+      if (message !== "The Friend approval has been done") return;
 
+      dispatch({ type: "CURRENT_USER", payload: user });
 
-  const {mutate:approveRequest} = useMutation(approveFriend,{
-    onSuccess:({message,user})=>{
-     
-      if(message !== 'The Friend approval has been done')return
-      dispatch({type:'CURRENT_USER',payload:user})
-        
       //Send back to navbar to remove approved request from list!
-      decreaseNotify()
-      
-      let notifyObj = {
-      reciever:notification.sender._id,
-      sender:excludeFieldsUserData(currentUser),
-      message}
-      
-      Socket.emit('notification',notifyObj) 
-    },
-    onError:error=>showBoundary(error)
-  
-  })
+      decreaseNotify();
 
-  const {mutate:unapprove} =  useMutation(unapproveFriend,{
-    onSuccess:({message,user})=>{
-  
-      if(message !== 'Request has been decline!')return
-      dispatch({type:'CURRENT_USER',payload:user})
-        
+      let notifyObj = {
+        reciever: notification.sender._id,
+        sender: excludeFieldsUserData(currentUser),
+        message,
+      };
+
+      Socket.emit("notification", notifyObj);
+    },
+    onError: (error) => showBoundary(error),
+  });
+
+  const { mutate: unapprove } = useMutation(unapproveFriend, {
+    onSuccess: ({ message, user }) => {
+      if (message !== "Request has been decline!") return;
+      dispatch({ type: "CURRENT_USER", payload: user });
+
       //Send back to navbar to remove declined request from list!
-      decreaseNotify()
-      
+      decreaseNotify();
+
       let notifyObj = {
-      reciever:notification.sender._id,
-      sender:excludeFieldsUserData(currentUser),
-      message}
-    
-      Socket.emit('notification',notifyObj) 
+        reciever: notification.sender._id,
+        sender: excludeFieldsUserData(currentUser),
+        message,
+      };
+
+      Socket.emit("notification", notifyObj);
     },
-    onError:error=>showBoundary(error)
-  
-  })  
+    onError: (error) => showBoundary(error),
+  });
 
-  useEffect(()=>{
-    if(notification.message === 'Friend request')return
-       setIsRequest(false)
-  },[])
+  useEffect(() => {
+    if (notification.message === "Friend request") return;
+    setIsRequest(false);
+  }, []);
 
+  const handleApprove = () => {
+    let obj = {
+      currentUserId: currentUser._id,
+      friendId: notification.sender._id,
+    };
+    approveRequest(obj);
+  };
 
- const handleApprove = () =>{
-  let obj = {currentUserId:currentUser._id,friendId:notification.sender._id}
-  approveRequest(obj)
- } 
+  const handleDecline = () => {
+    let obj = {
+      currentUserId: currentUser._id,
+      friendId: notification.sender._id,
+    };
+    unapprove(obj);
+  };
 
- const handleDecline = () =>{
-  let obj = {currentUserId:currentUser._id,friendId:notification.sender._id}
-  unapprove(obj)
- }
-   
   return (
     <>
-    <div className={styles.notificationMainDiv}>
+      <section className={styles.notificationMainDiv} aria-label="Notification">
+        <article className={styles.notifyPersonImgWrapper}>
+          <Image
+            style={{ borderRadius: "50%" }}
+            src={
+              notification.sender.image
+                ? notification.sender.image.url
+                : noAvatar
+            }
+            placeholder={notification?.image?.url ? "blur" : "empty"}
+            blurDataURL={notification?.image?.base64}
+            alt={notification.sender.name}
+            width={35}
+            height={35}
+          />
+        </article>
 
-      <div className={styles.notifyPersonImgWrapper}>
-        <Image
-         style={{borderRadius:'50%'}} 
-         src={notification.sender.image?
-         notification.sender.image:
-         noAvatar}
-         
-         alt={notification.sender.name}
-         width={35}
-         height={35}/>
-         
-      </div>
-     
-        {notification.sender.name} {notification.message}
-        {isRequest&&
-        <div className={styles.notificationButtonsDiv}>
-         <button 
-         onClick={handleDecline}>
-         Decline
-         </button>
+        <article className={styles.mainNotificationWrapper}>
+          <div className={styles.notificationDetails}>
+            <strong>{notification.sender.name}</strong> - {notification.message}
+          </div>
+          {isRequest && (
+            <div className={styles.notificationButtonsDiv}>
+              <button onClick={handleDecline}>Decline</button>
 
-         <button 
-         onClick={handleApprove}>
-         Approve
-         </button>
-        </div>}
-   
-    </div>
+              <button onClick={handleApprove}>Approve</button>
+            </div>
+          )}
+        </article>
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default Notification
+export default Notification;
