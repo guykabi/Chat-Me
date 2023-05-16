43572f0f6b94f1888e18aa1680c026813cd00ae9
Guy Kabilio <guyka00@gmail.com>
2023-05-16 15:24:05 +0300

    for global error handling - react-error-boundary

---
+++ client/components/errors/error/error.js
@@ -0,0 +1,64 @@
+import React,{useEffect, useState} from 'react'
+import styles from './error.module.css'
+import { push, useRouter } from "next/router";
+import Button from '../../UI/Button/button';
+
+const ErrorFallBack = ({ error, resetErrorBoundary }) => {
+
+const [isRefreshToken,setIsRefreshToken]=useState(false)
+const {reload} = useRouter()
+
+useEffect(()=>{
+    if(error?.response?.data?.message === 'Failed to authenticate refresh token'){
+        setIsRefreshToken(true)
+        setTimeout(() => {
+        resetErrorBoundary()
+        setIsRefreshToken(false)
+         push("/login");
+       }, 4000)
+    }
+},[]) 
+
+const directToLogin = () =>{
+  resetErrorBoundary()
+  push("/login")
+}
+
+
+  return (
+    <>
+     {isRefreshToken?
+       <section className={styles.errorWrapper}>
+        <pre>
+         Its been a while since you last signed in 
+        </pre>
+       </section> :  
+       <section className={styles.errorWrapper}>
+        <header>
+         <h2>Sorry</h2>
+         <h3> its seems that something went wrong</h3>
+        </header>
+        <section className={styles.btnsWrapper}>
+        <Button
+          className="primaryBtn"
+          text="Try again"
+          width="8"
+          height="10"
+          arialable="Refresh button"
+          onClick={()=>reload()}
+        />
+        <Button
+          className="primaryBtn"
+          text="Login page"
+          width="8"
+          height="10"
+          arialable="Direct to login button"
+          onClick={directToLogin}
+        />
+        </section>
+       </section>}
+    </>
+  )
+}
+
+export default ErrorFallBack
