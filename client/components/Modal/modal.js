ac7a408c2268473c97fb0987077472457a4a338b
Guy Kabilio <guyka00@gmail.com>
2023-05-16 15:20:11 +0300

    prettier

--- client/components/Modal/modal.js
+++ client/components/Modal/modal.js
@@ -1,65 +1,58 @@
-import React,{useEffect, useState} from 'react'
-import ReactDOM from 'react-dom'
-import styles from './modal.module.css'
-import {AiOutlineCloseCircle} from 'react-icons/ai'
+import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom";
+import styles from "./modal.module.css";
+import { AiOutlineCloseCircle } from "react-icons/ai";
 
-const Modal = ({show,onClose,children,title,isError,isFileMessage}) => {
-    const [onMount,setOnMount]=useState(false)
-     
-useEffect(()=>{
-  setOnMount(true)
-},[]) 
+const Modal = ({ show, onClose, children, title, isError, isFileMessage }) => {
+  const [onMount, setOnMount] = useState(false);
 
-const handleClose = (e) =>{
-  e.preventDefault()
-  if(!onClose)return
-  onClose()
-}
+  useEffect(() => {
+    setOnMount(true);
+  }, []);
 
-const modalContent = show ? (
+  const handleClose = (e) => {
+    e.preventDefault();
+    if (!onClose) return;
+    onClose();
+  };
+
+  const modalContent = show ? (
     <>
-    <article className={styles.overlay} > 
+      <article className={styles.overlay}>
+        <section className={styles.mainModal}>
+          {isFileMessage ? (
+            <div className={styles.deleteSign} onClick={handleClose}>
+              <AiOutlineCloseCircle />
+            </div>
+          ) : null}
 
-       <section className={styles.mainModal} >
-       
-       {isFileMessage?<div 
-        className={styles.deleteSign}
-        onClick={handleClose}>
-       <AiOutlineCloseCircle/>
-       </div>:null}
+          {!isError && !isFileMessage ? (
+            <div
+              className={styles.deleteModalSign}
+              role="button"
+              onClick={handleClose}
+            >
+              <AiOutlineCloseCircle />
+            </div>
+          ) : null}
+          {title ? (
+            <header className={styles.modalHeader}>{title}</header>
+          ) : null}
+          {isError ? <span className={styles.errorMsg}>{isError}</span> : null}
+          <main className={styles.modalContent}>{children}</main>
+        </section>
+      </article>
+    </>
+  ) : null;
 
-       {!isError&&!isFileMessage?<div 
-       className={styles.deleteModalSign}
-       role='button'
-       onClick={handleClose}>
-      <AiOutlineCloseCircle/>
-      </div>:null}
-           {title?
-           <header className={styles.modalHeader}>
-               {title}
-           </header>:null}
-           {isError?
-           <span className={styles.errorMsg}>
-            {isError}
-           </span>:null}
-           <main className={styles.modalContent}>
-             {children}
-           </main>
-       </section>
-    </article>
-   </>):null
- 
+  if (onMount) {
+    return ReactDOM.createPortal(
+      modalContent,
+      document.getElementById("modal-root")
+    );
+  } else {
+    return null;
+  }
+};
 
- if(onMount){
-     return ReactDOM.createPortal(
-         modalContent,
-         document.getElementById('modal-root')
-     ) 
-  } else{
-  return null
- }
-
-}
-
-
-export default Modal
+export default Modal;
