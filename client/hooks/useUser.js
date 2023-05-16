c7f9e47f4dc5b1783b9a6e7df346a7b3577b9999
Guy Kabilio <guyka00@gmail.com>
2023-05-16 15:16:09 +0300

    default value as null to onSuccess

--- client/hooks/useUser.js
+++ client/hooks/useUser.js
@@ -1,7 +1,7 @@
 import { useQuery } from "react-query"
 import { getUserDetails } from "../utils/apiUtils"
 
-export const useGetUser = (userId,enabled=true,onSuccess,onError)=>{
+export const useGetUser = (userId,enabled=true,onSuccess=null,onError)=>{
   return useQuery(['user'],()=>getUserDetails(userId),
   { 
     onSuccess,
