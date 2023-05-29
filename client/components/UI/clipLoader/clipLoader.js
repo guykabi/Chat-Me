6f01e0d5b9ea79bb722cebed38aaa3664548d0b3
Guy Kabilio <guyka00@gmail.com>
2023-05-29 10:29:17 +0300

    adding color variable

--- client/components/UI/clipLoader/clipLoader.js
+++ client/components/UI/clipLoader/clipLoader.js
@@ -1,9 +1,9 @@
 import {ClipLoader} from 'react-spinners' 
 
-export const Loader = ({size=25})=>{
+export const Loader = ({size=25,color='gray'})=>{
 
    return( <ClipLoader
-        color='gray'
+        color={color}
         size={size}
         aria-label="Loading Spinner"
         data-testid="loader"
