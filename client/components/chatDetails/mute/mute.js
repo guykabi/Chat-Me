b98a712a5c450fbfc7434056bb2225e5ac6c7c7b
Guy Kabilio <guyka00@gmail.com>
2023-05-29 10:33:29 +0300

    mute icon

---
+++ client/components/chatDetails/mute/mute.js
@@ -0,0 +1,50 @@
+import React,{useContext, useEffect, useState} from 'react'
+import { toast } from 'react-toastify';
+import styles from './mute.module.css'
+import {useMutation} from 'react-query'
+import {chatContext} from '../../../context/chatContext'
+import {muteConversation} from '../../../utils/apiUtils'
+import {GoUnmute, GoMute} from 'react-icons/go'
+
+
+const Mute = () => {
+
+  const {currentUser,currentChat,dispatch} = useContext(chatContext)
+  const [isMute,setIsMute]=useState()
+
+  const {mutate:muteChat} = useMutation(muteConversation,{
+     onSuccess:data=>{
+        setIsMute(data.mute)
+        dispatch({type:'CURRENT_USER',payload:data.user})
+     },
+     onError: () => toast.error('Unable to mute',{
+        position:'top-center',
+        theme:'colored'
+      })
+  })  
+
+
+ useEffect(()=>{
+  let isMute = currentUser?.mute?.some(chat=>chat === currentChat._id)
+  setIsMute(isMute)
+ },[]) 
+
+
+const handleMuteChat = () =>{
+  console.log('Here');
+    let body = {mute:currentChat._id} 
+    muteChat({userId:currentUser._id,body})
+}
+
+  return (
+    <>
+    <div className={styles.muteWrapper}>
+      {isMute?
+      <GoMute size={20} onClick={handleMuteChat}/>:
+      <GoUnmute size={20} onClick={handleMuteChat}/>}
+    </div>
+    </>
+  )
+}
+
+export default Mute
